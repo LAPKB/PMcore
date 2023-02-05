10cff3010684d63ea86abaa1574c26ce3010774c
Julian D. Otalvaro <juliandavid347@gmail.com>
2023-02-05 14:05:39 -0500

    code re-structured, exporting a prelude, reading base settings

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 pub mod base;
 
+pub mod prelude {
+    pub use crate::base::samplers::*;
+    pub use crate::base::*;
+}
+
 //Tests
 mod tests;
 
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,9 +1,9 @@
 #[cfg(test)]
-use super::*;
+use super::base::*;
 
 #[test]
 fn basic_sobol(){
-    assert_eq!(base::sobol_seq(5, vec![(0.,1.),(0.,1.),(0.,1.)], 347), ndarray::array![
+    assert_eq!(samplers::sobol(5, vec![(0.,1.),(0.,1.),(0.,1.)], 347), ndarray::array![
         [0.10731888, 0.14647412, 0.58510387],
         [0.9840305, 0.76333654, 0.19097507],
         [0.3847711, 0.73466134, 0.2616291],
@@ -14,11 +14,19 @@ fn basic_sobol(){
 
 #[test]
 fn scaled_sobol(){
-    assert_eq!(base::sobol_seq(5, vec![(0.,1.),(0.,2.),(-1.,1.)], 347), ndarray::array![
+    assert_eq!(samplers::sobol(5, vec![(0.,1.),(0.,2.),(-1.,1.)], 347), ndarray::array![
         [0.10731888, 0.29294825, 0.17020774],
         [0.9840305, 1.5266731, -0.61804986],
         [0.3847711, 1.4693227, -0.4767418],
         [0.70233, 0.82076526, 0.8317368],
         [0.60167587, 1.2342591, 0.25279427]
     ])
+}
+
+#[test]
+fn read_mandatory_settings(){
+    let settings = settings::read();
+    assert_eq!(settings.paths.data, "data.csv");
+    assert_eq!(settings.config.cycles, 1024);
+    assert_eq!(settings.config.engine, "NPAG");
 }
