5c71095812ab68e7bd439976f0c259a79352ff55
Julian D. Otalvaro <juliandavid347@gmail.com>
2023-02-07 12:11:51 -0500

    Simulator workinggit add .git add .git add .git add .git add .git add .git add .

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ pub mod base;
 pub mod prelude {
     pub use crate::base::lds::*;
     pub use crate::base::*;
+    pub use crate::base::datafile::Scenario;
+    pub use crate::base::simulator::Simulate;
+    pub use crate::base::simulator::Engine;
 }
 
 //Tests
