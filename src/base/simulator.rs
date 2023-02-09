2e559e56565685386bc7d4f7695479ba31511b5a
Julian D. Otalvaro <juliandavid347@gmail.com>
2023-02-09 13:21:00 -0500

    generating yout w/ interpolation @ time_obs

--- src/base/simulator.rs
+++ src/base/simulator.rs
@@ -1,6 +1,7 @@
 use crate::base::datafile::Scenario;
+use interp::interp_slice;
 pub trait Simulate{
-    fn simulate(&self, params: Vec<f64>, y0: Vec<f64>, tspan:[f64;2], scenario: &Scenario);
+    fn simulate(&self, params: Vec<f64>, y0: Vec<f64>, tspan:[f64;2], scenario: &Scenario) -> (Vec<f64>, Vec<f64>);
 }
 
 pub struct Engine<S>
@@ -19,13 +20,16 @@ where
             sim
         }
     }
-    pub fn run(&self, scenario: &Scenario){
-        self.sim.simulate(
+    pub fn run(&self, scenario: &Scenario) -> (Vec<f64>, Vec<f64>){
+        let (x_out, y_out) = self.sim.simulate(
             vec![0.1,2.0],
             vec![0.0,0.0],
             [scenario.time.first().unwrap().clone() as f64, scenario.time.last().unwrap().clone() as f64],
             scenario
         );
+
+        let y_intrp = interp_slice(&x_out, &y_out, &scenario.time_obs[..]);
+        (scenario.time_obs.clone(), y_intrp)
     }
 }
 
