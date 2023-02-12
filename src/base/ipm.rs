a2ef7666e7211a32bd4a078e59cc3c30a37f8453
Julian D. Otalvaro <juliandavid347@gmail.com>
2023-02-11 22:55:53 -0500

    more work into ipm

--- src/base/ipm.rs
+++ src/base/ipm.rs
@@ -1,8 +1,9 @@
 use std::error;
 
-use linfa_linalg::cholesky::Cholesky;
-use ndarray::{ArrayBase, Dim, OwnedRepr, Array, Zip, Array2};
+use linfa_linalg::{cholesky::{Cholesky, SolveC}, triangular::{SolveTriangular, Triangular}};
+use ndarray::{ArrayBase, Dim, OwnedRepr, Array, Zip, Array2, Ix2};
 use ndarray_stats::{QuantileExt, DeviationExt};
+
 
 pub fn burke(mut psi: ArrayBase<OwnedRepr<f64>,Dim<[usize; 2]>>) -> Result<(),Box<dyn error::Error>>{
     psi.par_mapv_inplace(|x| x.abs());
@@ -32,16 +33,13 @@ pub fn burke(mut psi: ArrayBase<OwnedRepr<f64>,Dim<[usize; 2]>>) -> Result<(),Bo
     let mut lam = ecol.clone();
     let w = 1./&plam;
     // dbg!(&w);
-    /// This will generate NaNs if there are infs in psi
-    /// because in Rust inf*0.0 = NaN
-    /// why am I getting infs in PSI?
     let ptw = psi.t().dot(&w);
     // dbg!(&ptw);
     let shrink = 2.*ptw.max().unwrap().clone();
     lam = lam * shrink;
     plam = plam * shrink;
-    let y = ecol - ptw;
-    let R = erow - &w*&plam;
+    let y = &ecol - ptw;
+    let R = &erow - &w*&plam;
     let normR = norm_inf(R);
 
     let sum_log_plam = plam.mapv(|x:f64| x.ln()).sum();
@@ -56,8 +54,8 @@ pub fn burke(mut psi: ArrayBase<OwnedRepr<f64>,Dim<[usize; 2]>>) -> Result<(),Bo
 
         let smu = sig * mu;
 
-        let inner = divide(&lam, &y);
-        let w_plam = divide(&plam, &w);
+        let inner = &lam/&y;//divide(&lam, &y);
+        let w_plam = &plam/&w;//divide(&plam, &w);
 
 
         let h = 
@@ -66,7 +64,25 @@ pub fn burke(mut psi: ArrayBase<OwnedRepr<f64>,Dim<[usize; 2]>>) -> Result<(),Bo
                 Array2::from_diag(&w_plam);
         
         let uph = h.cholesky()?;
-        dbg!(uph);
+        // dbg!(uph);
+        let smuyinv = smu*(&ecol/&y);
+        let rhsdw = &erow/&w - (psi.dot(&smuyinv));
+        let a = rhsdw.clone().into_shape((rhsdw.len().clone(),1))?;
+        //todo: cleanup this aux variable
+        // dbg!(uph.t().is_triangular(linfa_linalg::triangular::UPLO::Upper));
+
+        // uph.solve_into(rhsdw);
+        // dbg!(&rhsdw);
+        // dbg!(&a);
+        // // dbg!(uph.solve_triangular(&a, linfa_linalg::triangular::UPLO::Lower));
+        // // dbg!(uph.solvec(&a));
+        let x = uph.t().solve_triangular(&a, linfa_linalg::triangular::UPLO::Upper)?;
+        // dbg!(uph.t().dot(&x));
+        let dw = uph.solve_triangular(&x, linfa_linalg::triangular::UPLO::Lower)?;
+        // dbg!(dw);
+        let dy = - psi.t().dot(&dw);
+        dbg!(dy);
+
         
     // }
     
@@ -79,17 +95,17 @@ fn norm_inf(a: ArrayBase<OwnedRepr<f64>,Dim<[usize; 1]>>) -> f64{
     a.linf_dist(&zeros).unwrap()
 }
 
-fn divide(dividend: &ArrayBase<OwnedRepr<f64>,Dim<[usize; 1]>>, divisor: &ArrayBase<OwnedRepr<f64>,Dim<[usize; 1]>>) -> ArrayBase<OwnedRepr<f64>,Dim<[usize; 1]>>{
-    //check than dividend.len() == divisor.len()
-    //check than none of the elements of divisor == 0
-    //return a Result
-    let mut res:ArrayBase<OwnedRepr<f64>,Dim<[usize; 1]>> = Array::zeros(dividend.len());
-    Zip::from(&mut res)
-        .and(dividend)
-        .and(divisor)
-        .for_each(|res,dividend,divisor|{
-            *res = dividend/divisor;
-        });
-    res
-}
+// fn divide(dividend: &ArrayBase<OwnedRepr<f64>,Dim<[usize; 1]>>, divisor: &ArrayBase<OwnedRepr<f64>,Dim<[usize; 1]>>) -> ArrayBase<OwnedRepr<f64>,Dim<[usize; 1]>>{
+//     //check than dividend.len() == divisor.len()
+//     //check than none of the elements of divisor == 0
+//     //return a Result
+//     let mut res:ArrayBase<OwnedRepr<f64>,Dim<[usize; 1]>> = Array::zeros(dividend.len());
+//     Zip::from(&mut res)
+//         .and(dividend)
+//         .and(divisor)
+//         .for_each(|res,dividend,divisor|{
+//             *res = dividend/divisor;
+//         });
+//     res
+// }
 
