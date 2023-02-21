0679e2c665fd3a8446ce5ffc01ec87efa4615f37
Julian D. Otalvaro <juliandavid347@gmail.com>
2023-02-21 18:45:58 -0500

    Exploring how to handle multiple outputs

--- src/base/simulator.rs
+++ src/base/simulator.rs
@@ -1,7 +1,7 @@
 use crate::base::datafile::Scenario;
 use interp::interp_slice;
 pub trait Simulate{
-    fn simulate(&self, params: Vec<f64>, tspan:[f64;2], scenario: &Scenario) -> (Vec<f64>, Vec<f64>);
+    fn simulate(&self, params: Vec<f64>, tspan:[f64;2], scenario: &Scenario) -> (Vec<f64>, Vec<Vec<f64>>);
 }
 
 pub struct Engine<S>
@@ -20,14 +20,18 @@ where
             sim
         }
     }
-    pub fn pred(&self, scenario: &Scenario, params: Vec<f64>) -> Vec<f64>{
+    pub fn pred(&self, scenario: &Scenario, params: Vec<f64>) -> Vec<Vec<f64>>{
         let (x_out, y_out) = self.sim.simulate(
             params,
             [scenario.time.first().unwrap().clone() as f64, scenario.time_obs.last().unwrap().clone() as f64],
             scenario
         );
 
-        let y_intrp = interp_slice(&x_out, &y_out, &scenario.time_obs[..]);
+        let y_intrp: Vec<Vec<f64>> = vec![]; 
+        for out in y_out{
+            y_intrp.push(interp_slice(&x_out, &out, &scenario.time_obs[..]));
+        }
+        
         y_intrp
     }
 
