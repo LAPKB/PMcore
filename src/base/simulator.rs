ecb116339ffc3b457ed26be01bf3a552441fcf3e
Julian D. Otalvaro <juliandavid347@gmail.com>
2023-03-29 11:57:07 -0500

    NOT_WORKING: first iteration over the new data_parser previous to re-write the simulator

--- src/base/simulator.rs
+++ src/base/simulator.rs
@@ -1,12 +1,7 @@
 use crate::base::datafile::Scenario;
 use interp::interp_slice;
 pub trait Simulate {
-    fn simulate(
-        &self,
-        params: Vec<f64>,
-        tspan: [f64; 2],
-        scenario: &Scenario,
-    ) -> (Vec<f64>, Vec<Vec<f64>>);
+    fn simulate(&self, params: Vec<f64>, scenario: &Scenario) -> (Vec<f64>, Vec<Vec<f64>>);
 }
 
 pub struct Engine<S>
@@ -24,14 +19,7 @@ where
         Self { sim }
     }
     pub fn pred(&self, scenario: &Scenario, params: Vec<f64>) -> Vec<f64> {
-        let (x_out, y_out) = self.sim.simulate(
-            params,
-            [
-                *scenario.time.first().unwrap(),
-                *scenario.time.last().unwrap(),
-            ],
-            scenario,
-        );
+        let (x_out, y_out) = self.sim.simulate(params, scenario);
         let mut y_intrp: Vec<Vec<f64>> = vec![];
         for (i, out) in y_out.iter().enumerate() {
             y_intrp.push(interp_slice(
