fb6602d8d68735cf635ecac6155da713973544d3
Julian D. Otalvaro <juliandavid347@gmail.com>
2023-03-29 23:45:21 -0500

    it is working again, the results are not yet comparable and need to implement things like lag again

--- examples/two_eq_lag.rs
+++ examples/two_eq_lag.rs
@@ -2,82 +2,95 @@ use eyre::Result;
 use np_core::prelude::{datafile::get_cov, *};
 use ode_solvers::*;
 
-const STEP_SIZE: f64 = 0.1;
-
+#[derive(Debug, Clone)]
 struct Model<'a> {
     ka: f64,
     ke: f64,
     _v: f64,
     lag: f64,
     scenario: &'a Scenario,
-    _wt: f64,
+    infusions: Vec<Infusion>,
 }
 
 type State = Vector2<f64>;
 type Time = f64;
 
 impl ode_solvers::System<State> for Model<'_> {
-    fn system(&self, t: Time, y: &mut State, dy: &mut State) {
+    fn system(&self, t: Time, y: &State, dy: &mut State) {
         let ka = self.ka;
         let ke = self.ke;
+
         ///////////////////// USER DEFINED ///////////////
         dy[0] = -ka * y[0];
         dy[1] = ka * y[0] - ke * y[1];
         //////////////// END USER DEFINED ////////////////
-        for dose in &self.scenario.doses {
-            if (dose.time + self.lag) > t - (STEP_SIZE / 2.)
-                && (dose.time + self.lag) <= t + (STEP_SIZE / 2.)
-            {
-                y[dose.compartment] += dose.dose;
-            }
-        }
     }
 }
 
 struct Sim {}
 
+#[derive(Debug, Clone)]
+pub struct Infusion {
+    pub time: f64,
+    pub dur: f64,
+    pub amount: f64,
+    pub compartment: usize,
+}
+
 impl Simulate for Sim {
-    fn simulate(
-        &self,
-        params: Vec<f64>,
-        tspan: [f64; 2],
-        scenario: &Scenario,
-    ) -> (Vec<f64>, Vec<Vec<f64>>) {
-        // let wt = get_cov(&self.scenario.covariates, "WT".to_string()).unwrap().values.first().unwrap();
-        // let t = t - self.lag;
-        let system = Model {
+    fn simulate(&self, params: Vec<f64>, scenario: &Scenario) -> Vec<f64> {
+        let mut system = Model {
             ka: params[0],
             ke: params[1],
             _v: params[2],
             lag: params[3],
             scenario,
-            _wt: *get_cov(&scenario.covariates, "WT".to_string())
-                .unwrap()
-                .values
-                .first()
-                .unwrap(),
+            infusions: vec![],
         };
-        let y0 = State::new(0.0, 0.0);
-        let mut stepper = Rk4::new(system, tspan[0], y0, tspan[1], STEP_SIZE);
-        // let mut stepper = Dopri5::new(system, 0.0, 20.0, 1.0e-5, y0, 1.0e-14, 1.0e-14);
-        let _res = stepper.integrate();
-        let x = stepper.x_out().to_vec();
-        let y = stepper.y_out();
-        let mut yout: Vec<Vec<f64>> = vec![];
-        let v = params[2];
-
-        ///////////////////// ONE PER OUTPUT EQUATION ///////////////
-        let y0: Vec<f64> = y
-            .iter()
-            .map(|y| {
-                ///////////////////// USER DEFINED ///////////////
-                y[1] / v
-                //////////////// END USER DEFINED ////////////////
-            })
-            .collect();
-        yout.push(y0);
-        //////////////// END ONE PER OUTPUT EQUATION ////////////////
-        (x, yout)
+        let mut yout = vec![];
+        let mut y0 = State::new(0.0, 0.0);
+        let mut time = 0.0;
+        for block in &scenario.blocks {
+            for event in block {
+                if event.evid == 1 {
+                    if event.dur.unwrap_or(0.0) > 0.0 {
+                        //infusion
+                        system.infusions.push(Infusion {
+                            time: event.time,
+                            dur: event.dur.unwrap(),
+                            amount: event.dose.unwrap(),
+                            compartment: event.input.unwrap() - 1,
+                        });
+                    } else {
+                        //dose
+                        //we need to take lag into consideration
+                        y0[event.input.unwrap() - 1] += event.dose.unwrap();
+                    }
+                }
+                // let mut stepper = Dopri5::new(
+                //     system.clone(),
+                //     time,
+                //     event.time,
+                //     0.001,
+                //     y0,
+                //     1.0e-14,
+                //     1.0e-14,
+                // );
+                let mut stepper = Rk4::new(system.clone(), time, y0, event.time, 0.1);
+                let _res = stepper.integrate();
+                let y = stepper.y_out();
+                y0 = match y.last() {
+                    Some(y) => y.clone(),
+                    None => y0,
+                };
+                if event.evid == 0 {
+                    //obs
+                    yout.push(y0[event.outeq.unwrap() - 1] / params[1]);
+                }
+                time = event.time;
+            }
+        }
+        yout
     }
 }
 
