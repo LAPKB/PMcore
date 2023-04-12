56196cad9f36d2cd0f16f15a87f0a0541cec2d5b
Markus Herberg Hovd <m.h.hovd@farmasi.uio.no>
2023-04-12 11:17:15 +0200

    Working example

--- src/base/settings.rs
+++ src/base/settings.rs
@@ -50,6 +50,7 @@ pub struct Config {
     pub tui: bool,
     pub pmetrics_outputs: Option<bool>,
     pub exclude: Option<Array>,
+    pub stopfile: String,
 }
 
 pub fn read(filename: String) -> Data {
@@ -105,6 +106,7 @@ pub fn read(filename: String) -> Data {
             rv.push(value.as_float().unwrap());
         }
     }
+
     Data {
         computed: Computed {
             random: Range {
