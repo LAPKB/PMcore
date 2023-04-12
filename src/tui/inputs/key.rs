8912eb3a1033949ce86d1cfce9ade0e6fd0e38b4
mhovd <66058642+mhovd@users.noreply.github.com>
2023-04-12 09:57:06 +0200

    Temp

--- src/tui/inputs/key.rs
+++ src/tui/inputs/key.rs
@@ -74,6 +74,12 @@ impl Key {
         matches!(self, Key::Ctrl('c') | Key::Char('q') | Key::Esc)
     }
 
+    /// If end early
+    pub fn is_user_end_signal(&self) -> bool {
+        matches!(self, Key::Ctrl('e'))
+        user_end = true;
+    }
+
     /// Returns the function key corresponding to the given number
     ///
     /// 1 -> F1, etc...
