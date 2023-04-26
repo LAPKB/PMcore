3f5053b4a016b1c311f6d3a512b8ea021dc50269
Julian D. Otalvaro <juliandavid347@gmail.com>
2023-04-26 16:51:13 -0500

    GamLam working as it is in the Fortran code

--- src/base/optim.rs
+++ src/base/optim.rs
@@ -8,13 +8,13 @@ use super::sigma::ErrorPoly;
 struct GamLam<'a> {
     pred: &'a Array2<Array1<f64>>,
     scenarios: &'a Vec<Scenario>,
-    ep: ErrorPoly,
+    ep: ErrorPoly<'a>,
 }
 
 impl<'a> CostFunction for GamLam<'a> {
     type Param = f64;
     type Output = f64;
-    fn cost(&self, param: &Self::Param) -> Result<Self::Output, argmin::core::Error> {
+    fn cost(&self, _param: &Self::Param) -> Result<Self::Output, argmin::core::Error> {
         let prob = prob(self.pred, self.scenarios, &self.ep);
         Ok(prob.sum())
     }
