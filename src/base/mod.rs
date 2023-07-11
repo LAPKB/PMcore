289b3d6a4b518bec98b081f03b55f37e71b74b13
Markus Hovd <m.h.hovd@farmasi.uio.no>
2023-07-11 13:26:54 +0200

    Reads prior distributions with headers
    If prob is present, it is removed. Includes checks for parameters not present in both.

--- src/base/mod.rs
+++ src/base/mod.rs
@@ -4,18 +4,15 @@ use self::predict::{post_predictions, Engine, Predict};
 use self::settings::run::Data;
 use crate::prelude::start_ui;
 use crate::{algorithms::npag::npag, tui::state::AppState};
-use csv::{ReaderBuilder, WriterBuilder};
+use csv::WriterBuilder;
 use eyre::Result;
 use log::LevelFilter;
 use log4rs::append::file::FileAppender;
 use log4rs::config::{Appender, Config, Root};
 use log4rs::encode::pattern::PatternEncoder;
-
 use ndarray::{Array2, Axis};
-use ndarray_csv::Array2Reader;
-// use ndarray_csv::Array2Writer;
-use predict::sim_obs;
 
+use predict::sim_obs;
 use std::fs::{self, File};
 use std::thread::spawn;
 use std::time::Instant;
@@ -43,9 +40,73 @@ where
     let theta = match &settings.parsed.paths.prior_dist {
         Some(prior_path) => {
             let file = File::open(prior_path).unwrap();
-            let mut reader = ReaderBuilder::new().has_headers(false).from_reader(file);
-            let array_read: Array2<f64> = reader.deserialize_array2_dynamic().unwrap();
-            array_read
+            let mut reader = csv::ReaderBuilder::new()
+                .has_headers(true)
+                .from_reader(file);
+
+            let mut parameter_names: Vec<String> = reader
+                .headers()
+                .unwrap()
+                .clone()
+                .into_iter()
+                .map(|s| s.trim().to_owned())
+                .collect();
+
+            // Remove "prob" column if present
+            if let Some(index) = parameter_names.iter().position(|name| name == "prob") {
+                parameter_names.remove(index);
+            }
+
+            // Check and reorder parameters to match names in settings.parsed.random
+            let random_names: Vec<String> = settings
+                .parsed
+                .random
+                .iter()
+                .map(|(name, _)| name.clone())
+                .collect();
+
+            let mut reordered_indices: Vec<usize> = Vec::new();
+            for random_name in &random_names {
+                match parameter_names.iter().position(|name| name == random_name) {
+                    Some(index) => {
+                        reordered_indices.push(index);
+                    }
+                    None => {
+                        panic!("Parameter {} is not present in the CSV file.", random_name);
+                    }
+                }
+            }
+
+            // Check if there are remaining parameters not present in settings.parsed.random
+            if parameter_names.len() > random_names.len() {
+                let extra_parameters: Vec<&String> = parameter_names.iter().collect();
+                panic!(
+                    "Found parameters in the prior not present in configuration: {:?}",
+                    extra_parameters
+                );
+            }
+
+            // Read parameter values row by row, keeping only those associated with the reordered parameters
+            let mut theta_values = Vec::new();
+            for result in reader.records() {
+                let record = result.unwrap();
+                let values: Vec<f64> = reordered_indices
+                    .iter()
+                    .map(|&i| record[i].parse::<f64>().unwrap())
+                    .collect();
+                theta_values.push(values);
+            }
+
+            let n_points = theta_values.len();
+            let n_params = random_names.len();
+
+            // Convert nested Vec into a single Vec
+            let theta_values: Vec<f64> = theta_values.into_iter().flatten().collect();
+
+            let theta_array = Array2::from_shape_vec((n_points, n_params), theta_values)
+                .expect("Failed to create theta Array2");
+
+            theta_array
         }
         None => lds::sobol(
             settings.parsed.config.init_points,
@@ -53,6 +114,7 @@ where
             settings.parsed.config.seed,
         ),
     };
+
     let mut scenarios = datafile::parse(&settings.parsed.paths.data).unwrap();
     if let Some(exclude) = &settings.parsed.config.exclude {
         for val in exclude {
