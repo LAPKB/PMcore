b3e1699a00eedf17e703dcc97138bf50a0800b13
Markus Herberg Hovd <m.h.hovd@farmasi.uio.no>
2023-09-15 10:14:41 +0200

    TUI-related changes
    Moved some code to components.rs, which should serve as a "library" of components to assemble different TUIs.
    
    Some comments were added and removed.

--- src/tui/components.rs
+++ src/tui/components.rs
@@ -1,171 +1,8 @@
-use eyre::Result;
-use ratatui::{
-    backend::{Backend, CrosstermBackend},
-    layout::{Alignment, Constraint, Direction, Layout, Rect},
-    style::{Color, Modifier, Style},
-    symbols,
-    text::Span,
-    widgets::{
-        Axis, Block, BorderType, Borders, Cell, Chart, Dataset, GraphType, Paragraph, Row, Table,
-    },
-    Frame, Terminal,
-};
-use std::{
-    io::stdout,
-    time::{Duration, Instant},
-};
-use tokio::sync::mpsc::UnboundedReceiver;
+/// This file contains the different components of the TUI
+/// The purpose is to create common components with generic methods
 
-use super::{
-    inputs::{events::Events, InputEvent},
-    state::AppHistory,
-    App, AppReturn,
-};
 
-use crate::prelude::{output::NPCycle, Data};
-
-pub fn start_ui(mut rx: UnboundedReceiver<NPCycle>, settings: Data) -> Result<()> {
-    let stdout = stdout();
-    crossterm::terminal::enable_raw_mode()?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    let mut app = App::new();
-    let mut app_history = AppHistory::new();
-
-    terminal.clear()?;
-
-    // User event handler
-    let tick_rate = Duration::from_millis(200);
-    let mut events = Events::new(tick_rate);
-
-    let mut start_time = Instant::now();
-    let mut elapsed_time = Duration::from_secs(0);
-
-    // Main UI loop
-    loop {
-        app.state = match rx.try_recv() {
-            Ok(state) => state,
-            Err(_) => app.state,
-        };
-
-        // Stop incrementing elapsed time if conv is true
-        if app.state.stop_text.is_empty() {
-            let now = Instant::now();
-            if now.duration_since(start_time) > tick_rate {
-                elapsed_time += now.duration_since(start_time);
-                start_time = now;
-            }
-        }
-
-        // If we receive a new NPCycle, add it to the app_history
-        if !app_history
-            .cycles
-            .iter()
-            .any(|state| state.cycle == app.state.cycle)
-        {
-            app_history.add_cycle(app.state.clone());
-        }
-
-        // Draw the terminal
-        terminal
-            .draw(|rect| draw(rect, &app, &app_history, elapsed_time, &settings))
-            .unwrap();
-
-        // Handle inputs
-        let result = match events.recv() {
-            Some(InputEvent::Input(key)) => app.do_action(key),
-            None => AppReturn::Continue,
-        };
-        // Check if we should exit
-        if result == AppReturn::Exit {
-            break;
-        }
-    }
-
-    terminal.clear()?;
-    terminal.show_cursor()?;
-    crossterm::terminal::disable_raw_mode()?;
-    Ok(())
-}
-
-pub fn draw<B>(
-    rect: &mut Frame<B>,
-    app: &App,
-    app_history: &AppHistory,
-    elapsed_time: Duration,
-    settings: &Data,
-) where
-    B: Backend,
-{
-    let size = rect.size();
-    check_size(&size);
-
-    // Vertical layout (overall)
-    let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints(
-            [
-                Constraint::Length(3),
-                Constraint::Min(10),
-                Constraint::Min(5),
-            ]
-            .as_ref(),
-        )
-        .split(size);
-
-    // Title in first chunk (top)
-    let title = draw_title();
-    rect.render_widget(title, chunks[0]);
-
-    // Horizontal layout for three chunks (middle)
-    let body_chunk = chunks[1];
-    let body_layout = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints(
-            [
-                Constraint::Percentage(40),
-                Constraint::Percentage(40),
-                Constraint::Percentage(20),
-            ]
-            .as_ref(),
-        )
-        .split(body_chunk);
-
-    // First chunk
-    let status = draw_status(app, elapsed_time);
-    rect.render_widget(status, body_layout[0]);
-
-    // Second chunk
-    let options = draw_options(settings);
-    rect.render_widget(options, body_layout[1]);
-
-    // Third chunk
-    let commands = draw_commands(app);
-    rect.render_widget(commands, body_layout[2]);
-
-    // Prepare the data
-    let data: Vec<(f64, f64)> = app_history
-        .cycles
-        .iter()
-        .enumerate()
-        .map(|(x, entry)| (x as f64, entry.objf))
-        .collect();
-
-    let start_index = (data.len() as f64 * 0.1) as usize;
-
-    // Calculate data points and remove infinities
-    let mut norm_data: Vec<(f64, f64)> = data
-        .iter()
-        .filter(|&(_, y)| !y.is_infinite())
-        .skip(start_index)
-        .map(|&(x, y)| (x, y))
-        .collect();
-
-    let plot = draw_plot(&mut norm_data);
-    rect.render_widget(plot, chunks[2]);
-}
-
-fn draw_title<'a>() -> Paragraph<'a> {
+pub fn draw_title<'a>() -> Paragraph<'a> {
     Paragraph::new("NPcore Execution")
         .style(Style::default().fg(Color::LightCyan))
         .alignment(Alignment::Center)
@@ -177,7 +14,7 @@ fn draw_title<'a>() -> Paragraph<'a> {
         )
 }
 
-fn draw_status<'a>(app: &App, elapsed_time: Duration) -> Table<'a> {
+pub fn draw_status<'a>(app: &App, elapsed_time: Duration) -> Table<'a> {
     // Define (formatted) texts
     let cycle_text = format!("{}", app.state.cycle);
     let objf_text = format!("{:.5}", app.state.objf);
@@ -222,7 +59,7 @@ fn draw_status<'a>(app: &App, elapsed_time: Duration) -> Table<'a> {
         .column_spacing(1)
 }
 
-fn draw_options<'a>(settings: &Data) -> Table<'a> {
+pub fn draw_options<'a>(settings: &Data) -> Table<'a> {
     // Define the table data
 
     let cycles = settings.parsed.config.cycles.to_string();
@@ -271,7 +108,7 @@ fn draw_options<'a>(settings: &Data) -> Table<'a> {
         .column_spacing(1)
 }
 
-fn draw_commands(app: &App) -> Table {
+pub fn draw_commands(app: &App) -> Table {
     let key_style = Style::default().fg(Color::LightCyan);
     let help_style = Style::default().fg(Color::Gray);
 
@@ -304,7 +141,7 @@ fn draw_commands(app: &App) -> Table {
         .column_spacing(1)
 }
 
-fn draw_plot(norm_data: &mut [(f64, f64)]) -> Chart {
+pub fn draw_plot(norm_data: &mut [(f64, f64)]) -> Chart {
     // Find min and max values
     let (x_min, x_max) = norm_data
         .iter()
@@ -363,28 +200,4 @@ fn draw_plot(norm_data: &mut [(f64, f64)]) -> Chart {
                 .title(" Objective function ")
                 .borders(Borders::ALL),
         )
-}
-
-fn check_size(rect: &Rect) {
-    if rect.width < 52 {
-        // panic!("Require width >= 52, (got {})", rect.width);
-    }
-    if rect.height < 12 {
-        // panic!("Require height >= 12, (got {})", rect.height);
-    }
-}
-
-fn format_time(elapsed_time: std::time::Duration) -> String {
-    let elapsed_seconds = elapsed_time.as_secs();
-    let (elapsed, unit) = if elapsed_seconds < 60 {
-        (elapsed_seconds, "s")
-    } else if elapsed_seconds < 3600 {
-        let elapsed_minutes = elapsed_seconds / 60;
-        (elapsed_minutes, "m")
-    } else {
-        let elapsed_hours = elapsed_seconds / 3600;
-        (elapsed_hours, "h")
-    };
-    let time_text = format!("{}{}", elapsed, unit);
-    time_text
 }
--- src/tui/state.rs
+++ src/tui/state.rs
@@ -1,33 +1,6 @@
 use crate::prelude::output::NPCycle;
 
 #[derive(Debug, Clone)]
-// pub struct AppState {
-//     pub cycle: usize,
-//     pub objf: f64,
-//     pub delta_objf: f64,
-//     pub nspp: usize,
-//     pub stop_text: String,
-//     pub gamlam: f64,
-// }
-// impl AppState {
-//     pub fn new() -> Self {
-//         Self {
-//             cycle: 0,
-//             objf: 0.0,
-//             delta_objf: 0.0,
-//             nspp: 0,
-//             stop_text: "".to_string(),
-//             gamlam: 0.0,
-//         }
-//     }
-// }
-
-// impl Default for AppState {
-//     fn default() -> Self {
-//         Self::new()
-//     }
-// }
-
 pub struct AppHistory {
     pub cycles: Vec<NPCycle>,
 }
