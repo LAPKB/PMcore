ff7deb26ffb6541041fec03b6a30ca73abc1e13a
Julian D. Otalvaro <juliandavid347@gmail.com>
2023-09-22 14:18:38 -0500

    Migrated the Faer code to the new API

--- examples/big_qr_decomp.rs
+++ examples/big_qr_decomp.rs
@@ -1,4 +1,6 @@
 use csv::ReaderBuilder;
+use faer::{assert_matrix_eq, mat, IntoFaer, IntoNdarray};
+use faer::{Faer, Mat};
 use linfa_linalg::qr::QR;
 use ndarray::{Array2, Axis};
 use ndarray_csv::Array2Reader;
@@ -6,22 +8,47 @@ use np_core::prelude::{linalg::faer_qr_decomp, Permutation, PermuteArray};
 use std::fs::File;
 
 fn main() {
+    // let file = File::open("examples/data/n_psi.csv").unwrap();
+    // let mut reader = ReaderBuilder::new().has_headers(false).from_reader(file);
+    // let array_read: Array2<f64> = reader.deserialize_array2_dynamic().unwrap();
+    // let (r, perm) = faer_qr_decomp(&array_read);
+    // for i in 0..30 {
+    //     println!("{:?}", r[[i, i]]);
+    // }
+    // println!("-------------------------------------");
+    // let n_psi = array_read.permute_axis(
+    //     Axis(1),
+    //     &Permutation {
+    //         indices: perm.clone(),
+    //     },
+    // );
+    // let r = n_psi.qr().unwrap().into_r();
+    // for i in 0..30 {
+    //     println!("{:?}", r[[i, i]]);
+    // }
+
     let file = File::open("examples/data/n_psi.csv").unwrap();
     let mut reader = ReaderBuilder::new().has_headers(false).from_reader(file);
     let array_read: Array2<f64> = reader.deserialize_array2_dynamic().unwrap();
-    let (r, perm) = faer_qr_decomp(&array_read);
-    for i in 0..30 {
-        println!("{:?}", r[[i, i]]);
+
+    let x = Mat::from_fn(array_read.nrows(), array_read.ncols(), |i, j| {
+        array_read[[i, j]]
+    });
+    let x: Mat<f64> = x.into();
+    let qr = x.col_piv_qr();
+    let r_mat = qr.compute_r();
+    let (forward, inverse) = qr.col_permutation().into_arrays();
+    let mut r: Array2<f64> = Array2::zeros((r_mat.nrows(), r_mat.ncols()));
+    for i in 0..r.nrows() {
+        for j in 0..r.ncols() {
+            r[[i, j]] = if i <= j { r_mat.read(i, j) } else { 0.0 }
+            // r[[i, j]] = qr.read(i, j);
+        }
     }
-    println!("-------------------------------------");
-    let n_psi = array_read.permute_axis(
-        Axis(1),
-        &Permutation {
-            indices: perm.clone(),
-        },
-    );
-    let r = n_psi.qr().unwrap().into_r();
-    for i in 0..30 {
-        println!("{:?}", r[[i, i]]);
-    }
+    // for i in 0..30 {
+    //     println!("{:?}", r[[i, i]]);
+    // }
+    dbg!(forward);
+    dbg!(inverse);
+    dbg!(perm);
 }
--- src/base/linalg.rs
+++ src/base/linalg.rs
@@ -1,52 +1,19 @@
-use dyn_stack::{DynStack, GlobalMemBuffer, ReborrowMut, StackReq};
-use faer_core::{Mat, Parallelism};
-use faer_qr::col_pivoting::compute;
+use faer::{Faer, Mat};
 use ndarray::Array2;
 
 pub fn faer_qr_decomp(x: &Array2<f64>) -> (Array2<f64>, Vec<usize>) {
     let x = Mat::from_fn(x.nrows(), x.ncols(), |i, j| x[[i, j]]);
-
-    let rank = x.nrows().min(x.ncols());
-    let blocksize = compute::recommended_blocksize::<f64>(x.nrows(), x.ncols());
-    let mut mem =
-            GlobalMemBuffer::new(StackReq::any_of([
-                compute::qr_in_place_req::<f64>(
-                    x.nrows(),
-                    x.ncols(),
-                    blocksize,
-                    Parallelism::None,
-                    Default::default(),
-                )
-                .unwrap(),
-                faer_core::householder::apply_block_householder_sequence_transpose_on_the_left_in_place_req::<
-                    f64,
-                >(x.nrows(), blocksize, x.ncols())
-                .unwrap(),
-            ]));
-    let mut stack = DynStack::new(&mut mem);
-    let mut qr = x;
-    let mut h_factor = Mat::zeros(blocksize, rank);
-    let size = qr.nrows().min(qr.ncols());
-    let mut perm = vec![0; size];
-    let mut perm_inv = vec![0; size];
-    compute::qr_in_place(
-        qr.as_mut(),
-        h_factor.as_mut(),
-        &mut perm,
-        &mut perm_inv,
-        Parallelism::None,
-        stack.rb_mut(),
-        Default::default(),
-    );
-    let mut r: Array2<f64> = Array2::zeros((qr.nrows(), qr.ncols()));
-    for i in 0..qr.nrows() {
-        for j in 0..qr.ncols() {
-            r[[i, j]] = if i <= j { qr.read(i, j) } else { 0.0 }
-            // r[[i, j]] = qr.read(i, j);
+    let qr = x.col_piv_qr();
+    let r_mat = qr.compute_r();
+    let (forward, _inverse) = qr.col_permutation().into_arrays();
+    let mut r: Array2<f64> = Array2::zeros((r_mat.nrows(), r_mat.ncols()));
+    //TODO: migrate all my matrix operations to faer or look for faer's own conversion implementation
+    // https://github.com/sarah-ek/faer-rs/blob/main/faer/examples/conversions.rs
+    for i in 0..r.nrows() {
+        for j in 0..r.ncols() {
+            r[[i, j]] = if i <= j { r_mat.read(i, j) } else { 0.0 }
         }
     }
-    // dbg!(&r);
-    // dbg!(&perm);
-
+    let perm = Vec::from(forward);
     (r, perm)
 }
