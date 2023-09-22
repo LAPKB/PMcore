43f7d1f62d2e69d8818cb6b77e76737e724f2684
Julian D. Otalvaro <juliandavid347@gmail.com>
2023-09-22 16:01:35 -0500

    using faer's own transformation

--- src/base/linalg.rs
+++ src/base/linalg.rs
@@ -1,9 +1,9 @@
-use faer::{Faer, Mat};
+use faer::{Faer, IntoFaer, IntoNdarray};
 use ndarray::parallel::prelude::*;
 use ndarray::{Array2, Axis};
 
 pub fn qr(x: &Array2<f64>) -> (Array2<f64>, Vec<usize>) {
-    // TODO: we need more testing but this code seems to be not needed
+    // TODO: we need more testing but this code seems not to be needed
     // if n_psi.ncols() > n_psi.nrows() {
     //     let nrows = n_psi.nrows();
     //     let ncols = n_psi.ncols();
@@ -23,19 +23,11 @@ pub fn qr(x: &Array2<f64>) -> (Array2<f64>, Vec<usize>) {
     n_x.axis_iter_mut(Axis(0))
         .into_par_iter()
         .for_each(|mut row| row /= row.sum());
-    let mat_x = Mat::from_fn(n_x.nrows(), n_x.ncols(), |i, j| n_x[[i, j]]);
+    let mat_x = n_x.view().into_faer();
     let qr = mat_x.col_piv_qr();
     let r_mat = qr.compute_r();
     let (forward, _inverse) = qr.col_permutation().into_arrays();
-    //TODO: migrate all my matrix operations to faer or look for faer's own conversion implementation
-    // https://github.com/sarah-ek/faer-rs/blob/main/faer/examples/conversions.rs
-    // r_mat.as_ref().into_ndarray();
-    let mut r: Array2<f64> = Array2::zeros((r_mat.nrows(), r_mat.ncols()));
-    for i in 0..r.nrows() {
-        for j in 0..r.ncols() {
-            r[[i, j]] = if i <= j { r_mat.read(i, j) } else { 0.0 }
-        }
-    }
+    let r = r_mat.as_ref().into_ndarray().to_owned();
     let perm = Vec::from(forward);
     (r, perm)
 }
