a06cf6ef6d3b33b824993de30e6477943fcd65ed
Siel <Siel@users.noreply.github.com>
2023-10-02 18:22:14 +0000

    deploy: 269d2369cf1a7f8fd25e3b16e9548a8f81913235

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"npcore":["",[["algorithms",[],["npag.rs"]],["routines",[["evaluation",[],["ipm.rs","prob.rs","qr.rs","sigma.rs"]],["initialization",[],["sobol.rs"]],["optimization",[],["expansion.rs","optim.rs"]],["settings",[],["run.rs","simulator.rs"]],["simulation",[],["predict.rs","simulator.rs"]]],["datafile.rs","output.rs"]],["tests",[],["mod.rs"]],["tui",[["inputs",[],["events.rs","key.rs","mod.rs"]]],["actions.rs","mod.rs","state.rs","ui.rs"]]],["algorithms.rs","lib.rs"]]\
+"npcore":["",[["algorithms",[],["npag.rs","postprob.rs"]],["routines",[["evaluation",[],["ipm.rs","prob.rs","qr.rs","sigma.rs"]],["initialization",[],["sobol.rs"]],["optimization",[],["expansion.rs","optim.rs"]],["settings",[],["run.rs","simulator.rs"]],["simulation",[],["predict.rs","simulator.rs"]]],["datafile.rs","output.rs"]],["tests",[],["mod.rs"]],["tui",[["inputs",[],["events.rs","key.rs","mod.rs"]]],["actions.rs","mod.rs","state.rs","ui.rs"]]],["algorithms.rs","lib.rs"]]\
 }');
 createSourceSidebar();
