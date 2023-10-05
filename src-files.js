319504f9c7582672480265d3ec5df0fe3ee3e4ed
Siel <Siel@users.noreply.github.com>
2023-10-05 21:51:57 +0000

    deploy: 95cc98d90a2fa354f1f78f8d145d7394b270cdf4

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "npcore":["",[["algorithms",[],["npag.rs","postprob.rs"]],["routines",[["evaluation",[],["ipm.rs","prob.rs","qr.rs","sigma.rs"]],["initialization",[],["sobol.rs"]],["optimization",[],["expansion.rs","optim.rs"]],["settings",[],["run.rs","simulator.rs"]],["simulation",[],["predict.rs"]]],["datafile.rs","initialization.rs","output.rs"]],["tests",[],["mod.rs"]],["tui",[["inputs",[],["events.rs","key.rs","mod.rs"]]],["actions.rs","mod.rs","state.rs","ui.rs"]]],["algorithms.rs","entrypoints.rs","lib.rs","logger.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
