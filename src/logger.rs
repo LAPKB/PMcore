81cda083b31c20fb44d45fb7773dc1e5f3d1563f
Markus Hovd <m.h.hovd@farmasi.uio.no>
2023-11-11 17:14:49 +0100

    Nicer formatting

--- src/logger.rs
+++ src/logger.rs
@@ -1,5 +1,6 @@
 use crate::routines::settings::run::Data;
 use crate::tui::ui::Comm;
+use std::io::{self, Write};
 use tokio::sync::mpsc::UnboundedSender;
 use tracing_subscriber::fmt::time::FormatTime;
 use tracing_subscriber::fmt::{self};
@@ -7,7 +8,6 @@ use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
 use tracing_subscriber::registry::Registry;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::EnvFilter;
-use std::io::{Write, self};
 
 pub fn setup_log(settings: &Data, ui_tx: UnboundedSender<Comm>) {
     // Use the log level defined in configuration file, or default to info
@@ -32,7 +32,10 @@ pub fn setup_log(settings: &Data, ui_tx: UnboundedSender<Comm>) {
         .open(&settings.parsed.paths.log_out)
         .expect("Failed to open log file");
 
-    let file_layer = fmt::layer().with_writer(file).with_ansi(false);
+    let file_layer = fmt::layer()
+        .with_writer(file)
+        .with_ansi(false)
+        .with_timer(CompactTimestamp);
 
     // Define layer for stdout
     let stdout_layer = if !settings.parsed.config.tui {
@@ -56,7 +59,7 @@ pub fn setup_log(settings: &Data, ui_tx: UnboundedSender<Comm>) {
     let tui_layer = if settings.parsed.config.tui {
         let layer = fmt::layer()
             .with_writer(tui_writer_closure)
-            .with_ansi(true)
+            .with_ansi(false)
             .with_target(false)
             .with_timer(CompactTimestamp);
         Some(layer)
@@ -65,7 +68,11 @@ pub fn setup_log(settings: &Data, ui_tx: UnboundedSender<Comm>) {
     };
 
     // Combine layers with subscriber
-    subscriber.with(file_layer).with(stdout_layer).with(tui_layer).init();
+    subscriber
+        .with(file_layer)
+        .with(stdout_layer)
+        .with(tui_layer)
+        .init();
     tracing::debug!("Logging is configured with level: {}", log_level);
 }
 
@@ -89,7 +96,8 @@ impl Write for TuiWriter {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         let msg = String::from_utf8_lossy(buf);
         // Send the message through the channel
-        self.ui_tx.send(Comm::LogMessage(msg.to_string()))
+        self.ui_tx
+            .send(Comm::LogMessage(msg.to_string()))
             .map_err(|_| io::Error::new(io::ErrorKind::Other, "Failed to send log message"))?;
         Ok(buf.len())
     }
