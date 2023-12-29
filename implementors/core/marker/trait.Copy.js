485e58e2af75b6de68acec91d57cf7092fb9c892
Siel <Siel@users.noreply.github.com>
2023-12-29 13:06:31 +0000

    deploy: 63545af8dd46db1d80582dcfd5b302b52b6483a9

--- implementors/core/marker/trait.Copy.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"npcore":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"npcore/tui/inputs/key/enum.Key.html\" title=\"enum npcore::tui::inputs::key::Key\">Key</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"npcore/tui/actions/enum.Action.html\" title=\"enum npcore::tui::actions::Action\">Action</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ trait.impl/core/hash/trait.Hash.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"npcore":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"npcore/tui/inputs/key/enum.Key.html\" title=\"enum npcore::tui::inputs::key::Key\">Key</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
