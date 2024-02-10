28976c49f5c90c56475de867d77af5c30ddd2129
mhovd <mhovd@users.noreply.github.com>
2024-02-10 15:18:50 +0000

    deploy: 0c9f110b87bcf2d0d08a4c4aeba409680c4a8e8d

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var srcIndex = JSON.parse('{\
-"npcore":["",[["algorithms",[],["npag.rs","npod.rs","postprob.rs"]],["routines",[["condensation",[],["prune.rs"]],["evaluation",[],["ipm.rs","prob.rs","qr.rs","sigma.rs"]],["expansion",[],["adaptative_grid.rs"]],["initialization",[],["sobol.rs"]],["optimization",[],["d_optimizer.rs","optim.rs"]],["simulation",[],["predict.rs"]]],["datafile.rs","initialization.rs","output.rs","settings.rs"]],["tests",[],["mod.rs"]],["tui",[["inputs",[],["events.rs","key.rs","mod.rs"]]],["actions.rs","components.rs","mod.rs","state.rs","ui.rs"]]],["algorithms.rs","entrypoints.rs","lib.rs","logger.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["npcore",["",[["algorithms",[],["npag.rs","npod.rs","postprob.rs"]],["routines",[["condensation",[],["prune.rs"]],["evaluation",[],["ipm.rs","prob.rs","qr.rs","sigma.rs"]],["expansion",[],["adaptative_grid.rs"]],["initialization",[],["sobol.rs"]],["optimization",[],["d_optimizer.rs","optim.rs"]],["simulation",[],["predict.rs"]]],["datafile.rs","initialization.rs","output.rs","settings.rs"]],["tests",[],["mod.rs"]],["tui",[["inputs",[],["events.rs","key.rs","mod.rs"]]],["actions.rs","components.rs","mod.rs","state.rs","ui.rs"]]],["algorithms.rs","entrypoints.rs","lib.rs","logger.rs"]]]\
+]'));
 createSrcSidebar();
