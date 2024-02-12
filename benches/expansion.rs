e18d2190674fa1ff6407d8b9ee8576dabac1030c
Markus <66058642+mhovd@users.noreply.github.com>
2024-02-12 14:30:14 +0100

    Fix expansion benchmark
    Note: the large version takes a lot of time

--- benches/expansion.rs
+++ benches/expansion.rs
@@ -5,7 +5,7 @@ use pmcore::routines::expansion::adaptative_grid::adaptative_grid;
 // Define a benchmark function
 fn benchmark_adaptive_grid(c: &mut Criterion) {
     c.bench_function("adaptive_grid_small", |b| {
-        let mut theta = Array2::from_elem((50, 5), 0.5); // Small grid example
+        let mut theta = Array2::from_elem((100, 2), 0.5); // Small grid example
         let eps = 0.1;
         let ranges = vec![(0.0, 1.0), (0.0, 100.0)];
         let min_dist = 0.01;
@@ -23,7 +23,7 @@ fn benchmark_adaptive_grid(c: &mut Criterion) {
     c.bench_function("adaptive_grid_large", |b| {
         let mut theta = Array2::from_elem((1000, 10), 0.5); // Larger grid example
         let eps = 0.1;
-        let ranges = vec![(0.0, 1.0), (0.0, 100.0)];
+        let ranges = vec![(0.0, 1.0), (0.0, 100.0), (0.0, 100.0), (0.0, 100.0), (0.0, 100.0), (0.0, 100.0), (0.0, 100.0), (0.0, 100.0), (0.0, 100.0), (0.0, 100.0)];
         let min_dist = 0.01;
         
         b.iter(|| {
