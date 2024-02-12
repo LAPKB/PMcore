751fe9267f70ec0223435f50485621e15e9fe723
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-02-12 16:59:19 +0000

    h calculation working

--- src/routines/evaluation/ipm_faer.rs
+++ src/routines/evaluation/ipm_faer.rs
@@ -1,6 +1,9 @@
 use faer::{Faer, IntoFaer, IntoNdarray};
-use faer_core::{inner::DenseOwn, mat, scale, unzipped, zip, zipped, Mat, Matrix};
-use std::{borrow::BorrowMut, error, ops::Mul};
+use faer_core::{
+    inner::{DenseOwn, DiagRef},
+    mat, scale, unzipped, zip, zipped, Mat, Matrix,
+};
+use std::{borrow::BorrowMut, error, ops::Mul, os::unix::process};
 
 use linfa_linalg::{cholesky::Cholesky, triangular::SolveTriangular};
 use ndarray::{array, Array, Array2, ArrayBase, Dim, OwnedRepr};
@@ -42,6 +45,8 @@ pub fn burke(psi: &ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>) {
     let shrink = 2.0 * max_ptw;
     lam = scale(shrink) * lam;
     plam = scale(shrink) * plam;
+    // w = scale(1.0 / shrink) * w;
+    // ptw = scale(1.0 / shrink) * ptw;
     w = zipped!(w.as_ref()).map(|unzipped!(w_i)| *w_i / shrink);
     ptw = zipped!(ptw.as_ref()).map(|unzipped!(ptw_i)| *ptw_i / shrink);
     dbg!(&w);
@@ -54,5 +59,41 @@ pub fn burke(psi: &ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>) {
     dbg!(&y);
     dbg!(&r);
     dbg!(&norm_r);
+    let mut sum_log_plam = 0.0;
+    let mut sum_log_w = 0.0;
+    dbg!(w.ncols());
+    dbg!(w.nrows());
+    dbg!(&w);
+    for i in 0..plam.nrows() {
+        sum_log_plam += plam.get(i, 0).ln();
+    }
+    for i in 0..w.nrows() {
+        sum_log_w += w.get(i, 0).ln();
+    }
+    let mut gap = (sum_log_w + sum_log_plam).abs() / (1. + sum_log_plam);
+    dbg!(sum_log_w);
+    dbg!(gap);
+    let mut mu = (lam.transpose() * &y).get(0, 0) / col as f64;
+
+    dbg!(&mu);
+    while mu > eps || norm_r > eps || gap > eps {
+        let smu = sig * mu;
+        let inner = zipped!(lam.as_ref(), y.as_ref()).map(|unzipped!(lam_i, y_i)| *lam_i / *y_i);
+        dbg!(&inner);
+        let w_plam =
+            zipped!(plam.as_ref(), w.as_ref()).map(|unzipped!(plam_i, w_i)| *plam_i / *w_i);
+        dbg!(&w_plam);
+        let h: Matrix<DenseOwn<f64>> =
+            (&psi * inner.as_ref().col(0).column_vector_as_diagonal()) * &psi.transpose();
+        let mut aux: Matrix<DenseOwn<f64>> = Mat::zeros(row, row);
+        for i in 0..row {
+            let mut diag = aux.get_mut(i, i);
+            *diag = *w_plam.get(i, 0);
+        }
+        let h = h + aux;
+        dbg!(&h);
+        std::process::abort();
+    }
+
     // matrix_ops::Ok(())
 }
