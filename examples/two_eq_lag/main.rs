26fc36f577372f08e8f4cb65abfbd7e606bd7d6f
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-03-15 18:52:50 +0000

    tel is back

--- examples/two_eq_lag/main.rs
+++ examples/two_eq_lag/main.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
 use pmcore::prelude::*;
 
 // Constants for the absolute and relative tolerance for the dynamic steps used for solving the ODEs
@@ -9,7 +12,6 @@ const RTOL: f64 = 1e-4;
 // In brief, for up to 6 compartments, use VectorN<f64>, N being the number of compartments.
 // For more than 6 compartments, use `nalgebra::SVector<f64, N>`, where N is the number of compartments.
 type State = SVector<f64, 2>;
-
 // Time uses f64 precision
 type Time = f64;
 
@@ -22,7 +24,6 @@ struct Model {
     infusions: Vec<Infusion>,
     cov: Option<HashMap<String, CovLine>>,
 }
-
 // This is a helper function to get the parameter value by name
 impl Model {
     pub fn get_param(&self, str: &str) -> f64 {
@@ -33,23 +34,30 @@ impl Model {
 impl ode_solvers::System<Time, State> for Model {
     /// The system function, defining the ordinary differential equations (ODEs) to be solved
     fn system(&self, t: Time, x: &State, dx: &mut State) {
-        // Get the parameters from the model
+        // Get the parameters, covariates and secondary equations from the model
         let ka = self.get_param("ka");
         let ke = self.get_param("ke");
+        // let tlag1 = self.get_param("tlag1");
+        // let v = self.get_param("v");
+        let cov = self.cov.as_ref().unwrap();
+        let wt = match cov.get("WT") {
+            Some(x) => x.interp(t),
+            None => {
+                dbg!(cov);
+                panic!("WT not found in covariates")
+            }
+        };
+        // let africa = cov.get("AFRICA").unwrap().interp(t);
+        // let age = cov.get("AGE").unwrap().interp(t);
+        // let gender = cov.get("GENDER").unwrap().interp(t);
+        // let height = cov.get("HEIGHT").unwrap().interp(t);
 
-        let _wt = &self.cov.as_ref().unwrap().get("WT").unwrap().interp(t);
-
-        // Get the infusions that are active at time `t`
-        let mut rateiv = [0.0];
+        let mut rateiv = [0.0]; //TODO: hardcoded
         for infusion in &self.infusions {
             if t >= infusion.time && t <= (infusion.dur + infusion.time) {
-                rateiv[infusion.compartment] += infusion.amount / infusion.dur;
+                rateiv[infusion.compartment] = infusion.amount / infusion.dur;
             }
         }
-        // The ordinary differential equations (ODEs) are defined here
-        // This example is a one-compartmental model with first-order elimination, and intravenous infusions
-
-        ////// ODE //////
         dx[0] = -ka * x[0];
         dx[1] = ka * x[0] - ke * x[1];
     }
@@ -61,43 +69,64 @@ struct Ode {}
 impl<'a> Predict<'a> for Ode {
     type Model = Model;
     type State = State;
-    fn initial_system(&self, params: &Vec<f64>, scenario: Scenario) -> (Self::Model, Scenario) {
-        let params = HashMap::from(
-            [
-                ("ka".to_string(), params[0]),
-                ("ke".to_string(), params[1]),
-                ("lag".to_string(), params[2]),
-                ("v".to_string(), params[3]),
-            ]
-        );
-        let model = Model {
+    // This function is used initialize the system by setting parameter names and initial empty structs.
+    fn initial_system(&self, parameters: &Vec<f64>, scenario: Scenario) -> (Self::Model, Scenario) {
+        let mut params = HashMap::new();
+        // params.insert("ke".to_string(), params[0].clone());
+        // params.insert("v".to_string(), params[1].clone());
+        params.insert("ka".to_string(), parameters[0].clone());
+        params.insert("ke".to_string(), parameters[1].clone());
+        params.insert("tlag1".to_string(), parameters[2].clone());
+        params.insert("v".to_string(), parameters[3].clone());
+        let system = Model {
             params,
             _scenario: scenario.clone(), //TODO remove
             infusions: vec![],
             cov: None,
         };
-        let lag = model.get_param("lag");
+        let ka = system.get_param("ka");
+        let ke = system.get_param("ke");
+        let tlag1 = system.get_param("tlag1");
+        let v = system.get_param("v");
         (
-            model,
-            scenario.reorder_with_lag(vec![(lag, 1)]),
+            system,
+            // scenario.reorder_with_lag(vec![(0.0, 1)]))
+            scenario.reorder_with_lag(vec![(tlag1, 1)]),
         )
     }
-
     // This function is used to get the output from the model, defined by the output equations (outeq) supplied by the user
-    fn get_output(&self, time: f64, x: &Self::State, system: &Self::Model, outeq: usize) -> f64 {
-        // Get parameters from the model also used for calculating the output equations
+    fn get_output(&self, t: f64, x: &Self::State, system: &Self::Model, outeq: usize) -> f64 {
+        // let v = system.get_param("v");
+        // match outeq {
+        //     1 => x[0] / v,
+        //     _ => panic!("Invalid output equation"),
+        // }
+        // let ka = system.get_param("ka");
+        // let ke = system.get_param("ke");
+        // let tlag1 = system.get_param("tlag1");
         let v = system.get_param("v");
-        #[allow(unused_variables)]
-        let t = time;
+        // let cov = system.cov.as_ref().unwrap();
+        // let wt = match cov.get("WT") {
+        //     Some(x) => x.interp(t),
+        //     None => {
+        //         dbg!(cov);
+        //         panic!("WT not found in covariates")
+        //     }
+        // };
+        // let africa = cov.get("AFRICA").unwrap().interp(t);
+        // let age = cov.get("AGE").unwrap().interp(t);
+        // let gender = cov.get("GENDER").unwrap().interp(t);
+        // let height = cov.get("HEIGHT").unwrap().interp(t);
+
         match outeq {
-            1 => x[1] / v, // Concentration of the central compartment defined by the amount of drug, x[0], divided by the volume of the central compartment, v
+            1 => x[1] / v,
             _ => panic!("Invalid output equation"),
         }
     }
-
-    // Set the initial state of the compartments
+    // This function is used to initialize the comparments
+    //TODO: Handle non-zero initial conditions
     fn initial_state(&self) -> State {
-        State::new(0.0, 0.0)
+        State::default()
     }
     // Add any possible infusions
     fn add_infusion(&self, system: &mut Self::Model, infusion: Infusion) {
@@ -114,8 +143,10 @@ impl<'a> Predict<'a> for Ode {
     // Perform a "step" of the model, i.e. solve the ODEs from the current time to the next time
     // In the next step, we use this result as the initial state
     fn state_step(&self, x: &mut Self::State, system: &Self::Model, time: f64, next_time: f64) {
-        if time >= next_time {
+        if time > next_time {
             panic!("time error")
+        } else if time == next_time {
+            return;
         }
         let mut stepper = Dopri5::new(system.clone(), time, next_time, 1e-3, *x, RTOL, ATOL);
         let _res = stepper.integrate();
