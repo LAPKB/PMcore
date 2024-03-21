d03869eda2d7b2daf09adf786de25377fb5a0470
Markus <66058642+mhovd@users.noreply.github.com>
2024-03-21 14:54:13 +0100

    Update diffsol example

--- examples/diffsol.rs
+++ examples/diffsol.rs
@@ -2,7 +2,6 @@ use diffsol::*;
 use nalgebra::DVector;
 use std::rc::Rc;
 
-
 struct FloatRange {
     current: f64,
     end: f64,
@@ -33,12 +32,11 @@ impl Iterator for FloatRange {
     }
 }
 
-
 pub fn main() {
     type T = f64;
     type V = DVector<T>;
 
-    let ka = 1.0;
+    let ka = 0.8;
     let ke = 0.6;
     let p = V::from_vec(vec![ka, ke]);
     let mut problem = OdeSolverProblem::new_ode(
@@ -49,15 +47,15 @@ pub fn main() {
             y[1] = p[0] * x[0] - p[1] * x[1];
         },
         // The jacobian function `Jv`
-        |x: &V, p: &V, _t: T, _v: &V, y: &mut V| {
-            y[0] = -p[0] * x[0];
-            y[1] = p[0] * x[0] - p[1] * x[1];
+        |_x: &V, p: &V, _t: T, v: &V, y: &mut V| {
+            y[0] = -p[0] * v[0];
+            y[1] = p[0] * v[0] - p[1] * v[1];
         },
         // The initial condition(s)
         |_p: &V, _t: T| V::from_vec(vec![100.0, 0.0]),
         p,
     );
-    problem.rtol = 1.0e-4;
+    problem.rtol = 1.0e-6;
     problem.atol = Rc::new(V::from_vec(vec![1.0e-8, 1.0e-8]));
 
     let mut solver = Bdf::default();
@@ -65,11 +63,8 @@ pub fn main() {
     let mut state = OdeSolverState::new(&problem);
     solver.set_problem(&mut state, problem);
 
-
-    let frange = FloatRange::new(0.0, 12.0, 0.5);
-
-    let mut times: Vec<f64> = Vec::new();
-    let mut preds: Vec<(f64,f64)> = Vec::new();
+    let frange = FloatRange::new(0.0, 12.0, 1.0);
+    let mut result: Vec<(f64, f64, f64)> = Vec::new();
     for x in frange {
         let t = x;
         // Perform your operation with x
@@ -77,16 +72,14 @@ pub fn main() {
             solver.step(&mut state).unwrap();
         }
         let y = solver.interpolate(&state, t);
-        times.push(t);
-        preds.push((y[0], y[1]));
+        result.push((t, y[0], y[1]));
     }
 
-    print!("Time, Y[0], Y[1]\n");
-    for i in 0..times.len() {
-        print!("{:.2}, {:.2}, {:.2}\n", times[i], preds[i].0, preds[i].1);
+    print!("Time\t Y[0]\t Y[1]\n");
+    for i in 0..result.len() {
+        print!(
+            "{:.2}\t {:.2}\t {:.2}\n",
+            result[i].0, result[i].1, result[i].2
+        );
     }
-
-
 }
-
-
