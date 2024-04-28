77b8a08079eb57d80f0deca03b31bc89bc4a89f0
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-04-28 20:47:00 +0100

    new_simulator format

--- examples/new_simulator.rs
+++ examples/new_simulator.rs
@@ -10,18 +10,19 @@ fn main() {
     let subjects = data.get_subjects();
     let first_subject = *subjects.first().unwrap();
 
-    let diffeq = |x: &V, p: &V, _t: T, dx: &mut V, rateiv: V, _cov: &Covariates| {
-        // fetch_cov!(cov, t, creat);
-        fetch_params!(p, ka, ke, _v);
-        dx[0] = -ka * x[0];
-        dx[1] = ka * x[0] - ke * x[1] + rateiv[0];
-    };
-    let init = |_p: &V, _t: T| V::from_vec(vec![0.1, 0.2]);
-    let out = |x: &V, p: &V, _t: T, _cov: &Covariates| {
-        fetch_params!(p, _ka, _ke, v);
-        V::from_vec(vec![x[1] / v])
-    };
-    let ode = Equation::new_ode(diffeq, init, out);
+    let ode = Equation::new_ode(
+        |x: &V, p: &V, _t: T, dx: &mut V, rateiv: V, _cov: &Covariates| {
+            // fetch_cov!(cov, t, creat);
+            fetch_params!(p, ka, ke, _v);
+            dx[0] = -ka * x[0];
+            dx[1] = ka * x[0] - ke * x[1] + rateiv[0];
+        },
+        |_p: &V, _t: T| V::from_vec(vec![0.1, 0.2]),
+        |x: &V, p: &V, _t: T, _cov: &Covariates| {
+            fetch_params!(p, _ka, _ke, v);
+            V::from_vec(vec![x[1] / v])
+        },
+    );
 
     let sim = ode.simulate_subject(first_subject, &vec![0.9, 0.1, 50.0]);
     dbg!(sim);
