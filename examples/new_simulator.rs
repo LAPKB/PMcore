e640c6536bb87facd803bf6b3b4c906750093226
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-04-28 20:38:01 +0100

    new_simulator example

--- examples/new_simulator.rs
+++ examples/new_simulator.rs
@@ -1,33 +1,17 @@
-use diffsol::{
-    jacobian::{find_non_zero_entries, JacobianColoring},
-    matrix::Matrix,
-    ode_solver::equations::OdeEquationsStatistics,
-    ode_solver::method::OdeSolverMethod,
-    ode_solver::problem::OdeSolverProblem,
-    op::{closure::Closure, Op},
-    vector::{Vector, VectorIndex},
-    Bdf, OdeBuilder, OdeEquations, Result, Zero,
-};
-use pmcore::routines::data::{
-    parse_pmetrics::read_pmetrics, CovariateTrait, Covariates, CovariatesTrait, DataTrait,
-    Infusion, OccasionTrait,
-};
+use diffsol::vector::Vector;
+use pmcore::routines::data::{parse_pmetrics::read_pmetrics, Covariates, DataTrait};
 use pmcore::{prelude::*, simulator::Equation};
-use std::{cell::RefCell, path::Path, rc::Rc};
+use std::path::Path;
+type T = f64;
+type V = faer::Col<T>;
+
 fn main() {
     let data = read_pmetrics(Path::new("examples/data/bimodal_ke_blocks.csv")).unwrap();
     let subjects = data.get_subjects();
     let first_subject = *subjects.first().unwrap();
-    let occasion = first_subject.occasions.first().unwrap();
-    let covariates = occasion.covariates.clone();
-    let infusions = occasion.get_infusions();
-    println!("{}", data);
 
-    type T = f64;
-    type V = faer::Col<T>;
-    type M = faer::Mat<T>;
-    let diffeq = |x: &V, p: &V, t: T, dx: &mut V, rateiv: V, cov: &Covariates| {
-        fetch_cov!(cov, t, creat);
+    let diffeq = |x: &V, p: &V, _t: T, dx: &mut V, rateiv: V, _cov: &Covariates| {
+        // fetch_cov!(cov, t, creat);
         fetch_params!(p, ka, ke, _v);
         dx[0] = -ka * x[0];
         dx[1] = ka * x[0] - ke * x[1] + rateiv[0];
