6073e89903f5139cb7b94b28ce7c0837d5bd9961
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-04-30 18:37:57 +0100

    two_equations_with_absorption

--- src/simulator/analytical/mod.rs
+++ src/simulator/analytical/mod.rs
@@ -114,3 +114,59 @@ pub fn two_compartments(x: &V, p: &V, t: T, rateiv: V, _cov: &Covariates) -> V {
 
     non_zero + infusion
 }
+
+///
+/// Analytical for two compartment with absorption
+/// Assumptions:
+///   - p is a vector of length 4 with ke, ka, kcp and kpc in that order
+///   - rateiv is a vector of length 1 with the value of the infusion rate (only one drug)
+///   - x is a vector of length 2
+///   - covariates are not used
+///
+pub fn two_compartments_with_absorption(x: &V, p: &V, t: T, rateiv: V, _cov: &Covariates) -> V {
+    let ke = p[0];
+    let ka = p[1];
+    let kcp = p[2];
+    let kpc = p[3];
+    let mut xout = x.clone();
+
+    let sqrt = (ke + kcp + kpc).powi(2) - 4.0 * ke * kpc;
+    if sqrt < 0.0 {
+        panic!("Imaginary solutions, program stopped!");
+    }
+    let sqrt = sqrt.sqrt();
+    let l1 = (ke + kcp + kpc + sqrt) / 2.0;
+    let l2 = (ke + kcp + kpc - sqrt) / 2.0;
+    let non_zero = faer::scale(1. / (l1 - l2))
+        * faer::mat![
+            [
+                (l1 - kpc) * (-l1 * t).exp() + (kpc - l2) * (-l2 * t).exp(),
+                -kpc * (-l1 * t).exp() + kpc * (-l2 * t).exp(),
+            ],
+            [
+                kcp * (-l1 * t).exp() + kcp * (-l2 * t).exp(),
+                (l1 - ke - kcp) * (-l1 * t).exp() + (-ke + kcp - l2) * (-l2 * t).exp()
+            ]
+        ]
+        * faer::col![x[1], x[2]];
+    let infusion = faer::scale(rateiv[0] / (l1 - l2))
+        * faer::col![
+            ((l1 - kpc) / l1) * (1. - (-l1 * t).exp()) + ((kpc - l2) / l2) * (1. - (-l2 * t).exp()),
+            (-kpc / l1) * (1. - (-l1 * t).exp()) + (kpc / l2) * (1. - (-l2 * t).exp()),
+        ];
+
+    let absorption = faer::scale(ka * x[0] / (l1 - l2))
+        * faer::col![
+            ((l1 - kpc) / (ka - l1)) * ((-l1 * t).exp() - (-ka * t).exp())
+                + ((kpc - l2) / (ka - l2)) * ((-l2 * t).exp() - (-ka * t).exp()),
+            (-kpc / (ka - l1)) * ((-l1 * t).exp() - (-ka * t).exp())
+                + (kpc / (ka - l2)) * ((-l2 * t).exp() - (-ka * t).exp()),
+        ];
+    let aux = non_zero + infusion + absorption;
+    xout[0] = x[0] * (-ka * t).exp();
+
+    xout[1] = aux[0];
+    xout[2] = aux[1];
+
+    xout
+}
