197579d71e2d6b39e390ef7e80fc4252bdb2e284
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-05-04 23:09:30 +0100

    analytical bimodal_ke

--- examples/bimodal_ke/main.rs
+++ examples/bimodal_ke/main.rs
@@ -1,4 +1,10 @@
-use pmcore::prelude::*;
+use pmcore::{
+    prelude::*,
+    simulator::{
+        analytical::{one_compartment, one_compartment_with_absorption},
+        Equation, V,
+    },
+};
 
 // Constants for the absolute and relative tolerance for the dynamic steps used for solving the ODEs
 const ATOL: f64 = 1e-4;
@@ -112,12 +118,31 @@ impl<'a> Predict<'a> for Ode {
 }
 
 fn main() -> Result<()> {
-    unimplemented!();
-    // // Main entrypoint, see `entrypoints.rs` for more details
-    // let _result = start(
-    //     Engine::new(Ode {}),
-    //     "examples/bimodal_ke/config.toml".to_string(),
-    // )?;
-
-    // Ok(())
+    // let eq = Equation::new_ode(
+    //     |x, p, _t, dx, rateiv, _cov| {
+    //         // fetch_cov!(cov, t, wt);
+    //         fetch_params!(p, ke, _v);
+    //         dx[0] = -ke * x[0] + rateiv[0];
+    //     },
+    //     |_p| lag! {},
+    //     |_p| fa! {},
+    //     |_p, _t, _cov| V::from_vec(vec![0.0, 0.0]),
+    //     |x, p, _t, _cov| {
+    //         fetch_params!(p, _ke, v);
+    //         V::from_vec(vec![x[0] / v])
+    //     },
+    // );
+    let eq = Equation::new_analytical(
+        one_compartment,
+        |_p, _cov| {},
+        |p| lag! {},
+        |_p| fa! {},
+        |_p, _t, _cov| V::from_vec(vec![0.0]),
+        |x, p, _t, _cov| {
+            fetch_params!(p, _ke, v);
+            V::from_vec(vec![x[0] / v])
+        },
+    );
+    let _result = start(eq, "examples/bimodal_ke/config.toml".to_string())?;
+    Ok(())
 }
