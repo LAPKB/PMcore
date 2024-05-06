4a79327b5ccd69d4f6448f6eacbb8aa6554f9b1d
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-05-06 15:42:08 +0100

    tel working

--- examples/two_eq_lag/main.rs
+++ examples/two_eq_lag/main.rs
@@ -7,38 +7,38 @@ use pmcore::{
 };
 
 fn main() -> Result<()> {
-    // let eq = Equation::new_ode_solvers(
-    //     |x, p, _t, dx, rateiv, _cov| {
-    //         fetch_params!(p, ke, ka, _v, _tlag);
-    //         dx[0] = -ka * x[0];
-    //         dx[1] = ka * x[0] - ke * x[1];
-    //     },
-    //     |p| {
-    //         fetch_params!(p, _ke, _ka, _v, tlag);
-    //         lag! {0=>tlag}
-    //     },
-    //     |_p| fa! {},
-    //     |_p, _t, _cov, _x| {},
-    //     |x, p, _t, _cov, y| {
-    //         fetch_params!(p, _ke, v);
-    //         y[0] = x[1] / v;
-    //     },
-    // );
-
-    let eq = Equation::new_analytical(
-        one_compartment_with_absorption,
-        |_p, _cov| {},
+    let eq = Equation::new_ode_solvers(
+        |x, p, _t, dx, rateiv, _cov| {
+            fetch_params!(p, ka, ke, _tlag, _v);
+            dx[0] = -ka * x[0];
+            dx[1] = ka * x[0] - ke * x[1];
+        },
         |p| {
-            fetch_params!(p, _ke, _ka, _v, tlag);
+            fetch_params!(p, _ka, _ke, tlag, _v);
             lag! {0=>tlag}
         },
         |_p| fa! {},
         |_p, _t, _cov, _x| {},
         |x, p, _t, _cov, y| {
-            fetch_params!(p, _ke, _ka, v, _tlag);
+            fetch_params!(p, _ka, _ke, _tlag, v);
             y[0] = x[1] / v;
         },
     );
+
+    // let eq = Equation::new_analytical(
+    //     one_compartment_with_absorption,
+    //     |_p, _cov| {},
+    //     |p| {
+    //         fetch_params!(p, _ka, _ke, tlag, _v);
+    //         lag! {0=>tlag}
+    //     },
+    //     |_p| fa! {},
+    //     |_p, _t, _cov, _x| {},
+    //     |x, p, _t, _cov, y| {
+    //         fetch_params!(p, _ka, _ke, _tlag, v);
+    //         y[0] = x[1] / v;
+    //     },
+    // );
     let _result = start(eq, "examples/two_eq_lag/config.toml".to_string())?;
 
     Ok(())
