e8fe85df3060cef4cd8866e98ac9cc18223a2a3b
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-05-07 17:54:25 +0100

    bump diffsol and its new interface

--- src/simulator/ode/diffsol_traits.rs
+++ src/simulator/ode/diffsol_traits.rs
@@ -1,141 +1,65 @@
 use crate::prelude::data::{Covariates, Infusion};
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use diffsol::{
     matrix::Matrix,
-    ode_solver::problem::OdeSolverProblem,
+    ode_solver::{equations::OdeSolverEquations, problem::OdeSolverProblem},
     op::{unit::UnitCallable, Op},
     vector::Vector,
-    OdeBuilder, OdeEquations,
+    OdeEquations,
 };
-use num_traits::Zero;
+
 use std::rc::Rc;
 
 use super::closure::PMClosure;
 
-pub struct OdePmSolverEquationsMassI<M, F, I>
-where
-    M: Matrix,
-    F: Fn(&M::V, &M::V, M::T, &mut M::V, M::V, &Covariates),
-    I: Fn(&M::V, M::T) -> M::V,
-{
-    rhs: Rc<PMClosure<M, F>>,
-    mass: Rc<UnitCallable<M>>,
+pub fn build_pm_ode<M, F, I, Ite, T>(
+    rhs: F,
     init: I,
-    p: Rc<M::V>,
-}
-
-impl<M, F, I> OdePmSolverEquationsMassI<M, F, I>
+    p: M::V,
+    t0: f64,
+    h0: f64,
+    rtol: f64,
+    atol: Ite,
+    cov: Covariates,
+    infusions: Vec<Infusion>,
+) -> Result<OdeSolverProblem<OdeSolverEquations<M, PMClosure<M, F>, I>>>
 where
     M: Matrix,
     F: Fn(&M::V, &M::V, M::T, &mut M::V, M::V, &Covariates),
     I: Fn(&M::V, M::T) -> M::V,
+    Ite: IntoIterator<Item = T>,
+    f64: From<T>,
 {
-    pub fn new_pm_ode(
-        rhs: F,
-        init: I,
-        p: M::V,
-        t0: M::T,
-        calculate_sparsity: bool,
-        covariates: Covariates,
-        infusions: Vec<Infusion>,
-    ) -> Self {
-        let y0 = init(&p, M::T::zero());
-        let nstates = y0.len();
-        let p = Rc::new(p);
-        let mut rhs = PMClosure::new(rhs, nstates, nstates, p.clone(), covariates, infusions);
-        if calculate_sparsity {
-            rhs.calculate_sparsity(&y0, t0);
-        }
-        let mass = UnitCallable::<M>::new(nstates);
-        let rhs = Rc::new(rhs);
-        let mass = Rc::new(mass);
-        Self {
-            rhs,
-            mass,
-            init,
-            p: p.clone(),
-        }
-    }
+    let p = Rc::new(p);
+    let t0 = M::T::from(t0);
+    let y0 = (init)(&p, t0);
+    let nstates = y0.len();
+    let rhs = PMClosure::new(rhs, nstates, nstates, p.clone(), cov, infusions);
+    let mass = Rc::new(UnitCallable::new(nstates));
+    let rhs = Rc::new(rhs);
+    let eqn = OdeSolverEquations::new(rhs, mass, None, init, p, false);
+    let atol = atol.into_iter().map(|x| f64::from(x)).collect();
+    let atol = build_atol(atol, eqn.rhs().nstates())?;
+    Ok(OdeSolverProblem::new(
+        eqn,
+        M::T::from(rtol),
+        atol,
+        t0,
+        M::T::from(h0),
+    ))
 }
-
-impl<M, F, I> OdeEquations for OdePmSolverEquationsMassI<M, F, I>
-where
-    M: Matrix,
-    F: Fn(&M::V, &M::V, M::T, &mut M::V, M::V, &Covariates),
-    I: Fn(&M::V, M::T) -> M::V,
-{
-    type T = M::T;
-    type V = M::V;
-    type M = M;
-    type Rhs = PMClosure<M, F>;
-    type Mass = UnitCallable<M>;
-    fn mass(&self) -> &Rc<Self::Mass> {
-        &self.mass
-    }
-
-    fn rhs(&self) -> &Rc<Self::Rhs> {
-        &self.rhs
-    }
-
-    fn is_mass_constant(&self) -> bool {
-        true
-    }
-
-    fn init(&self, t: Self::T) -> Self::V {
-        let p = self.p.as_ref();
-        (self.init)(p, t)
-    }
-
-    fn set_params(&mut self, p: Self::V) {
-        self.p = Rc::new(p);
-    }
-}
-
-pub trait BuildPmOde {
-    fn build_pm_ode<M, F, I>(
-        self,
-        rhs: F,
-        init: I,
-        cov: Covariates,
-        infusions: Vec<Infusion>,
-    ) -> Result<OdeSolverProblem<OdePmSolverEquationsMassI<M, F, I>>>
-    where
-        M: Matrix,
-        F: Fn(&M::V, &M::V, M::T, &mut M::V, M::V, &Covariates),
-        I: Fn(&M::V, M::T) -> M::V;
-}
-
-impl BuildPmOde for OdeBuilder {
-    fn build_pm_ode<M, F, I>(
-        self,
-        rhs: F,
-        init: I,
-        cov: Covariates,
-        infusions: Vec<Infusion>,
-    ) -> Result<OdeSolverProblem<OdePmSolverEquationsMassI<M, F, I>>>
-    where
-        M: Matrix,
-        F: Fn(&M::V, &M::V, M::T, &mut M::V, M::V, &Covariates),
-        I: Fn(&M::V, M::T) -> M::V,
-    {
-        let p = Self::build_p(self.p);
-        let eqn = OdePmSolverEquationsMassI::new_pm_ode(
-            rhs,
-            init,
-            p,
-            M::T::from(self.t0),
-            self.use_coloring,
-            cov,
-            infusions,
-        );
-
-        let atol = Self::build_atol(self.atol, eqn.rhs().nstates())?;
-        Ok(OdeSolverProblem::new(
-            eqn,
-            M::T::from(self.rtol),
-            atol,
-            M::T::from(self.t0),
-            M::T::from(self.h0),
+fn build_atol<V: Vector>(atol: Vec<f64>, nstates: usize) -> Result<V> {
+    if atol.len() == 1 {
+        Ok(V::from_element(nstates, V::T::from(atol[0])))
+    } else if atol.len() != nstates {
+        Err(anyhow::anyhow!(
+            "atol must have length 1 or equal to the number of states"
         ))
+    } else {
+        let mut v = V::zeros(nstates);
+        for (i, &a) in atol.iter().enumerate() {
+            v[i] = V::T::from(a);
+        }
+        Ok(v)
     }
 }
--- src/simulator/ode/mod.rs
+++ src/simulator/ode/mod.rs
@@ -2,10 +2,12 @@ pub mod closure;
 pub mod diffsol_traits;
 use crate::{
     prelude::data::{Covariates, Infusion},
-    simulator::{ode::diffsol_traits::BuildPmOde, DiffEq, M, T, V},
+    simulator::{DiffEq, M, T, V},
 };
 
-use diffsol::{ode_solver::method::OdeSolverMethod, Bdf, OdeBuilder};
+use diffsol::{ode_solver::method::OdeSolverMethod, Bdf};
+
+use self::diffsol_traits::build_pm_ode;
 
 const RTOL: f64 = 1e-4;
 const ATOL: f64 = 1e-4;
@@ -20,19 +22,18 @@ pub fn simulate_ode_event(
     ti: f64,
     tf: f64,
 ) -> V {
-    let problem = OdeBuilder::new()
-        .t0(ti)
-        .rtol(RTOL)
-        .atol([ATOL])
-        .p(support_point.to_owned())
-        .build_pm_ode::<M, _, _>(
-            diffeq.clone(),
-            move |_p: &V, _t: T| x.clone(),
-            cov.clone(),
-            infusions.clone(),
-        )
-        .unwrap();
-
+    let problem = build_pm_ode::<M, _, _, _, _>(
+        diffeq.clone(),
+        move |_p: &V, _t: T| x.clone(),
+        V::from_vec(support_point.to_vec()),
+        ti,
+        1.0,
+        RTOL,
+        [ATOL],
+        cov.clone(),
+        infusions.clone(),
+    )
+    .unwrap();
     let mut solver = Bdf::default();
     solver.solve(&problem, tf).unwrap()
 }
