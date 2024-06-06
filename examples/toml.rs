15289563542888962d0e273eb2769ac274600726
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-06-06 20:19:22 +0100

    trying to break it

--- examples/toml.rs
+++ examples/toml.rs
@@ -3,7 +3,15 @@ use settings::read_settings;
 
 fn main() {
     let path = "examples/bimodal_ke/config.toml".to_string();
-    let s = read_settings(path).unwrap();
-
-    dbg!(s.random.parameters);
+    for i in 0..1000 {
+        let s = read_settings(path.clone()).unwrap();
+        let keys: Vec<&String> = s.random.parameters.keys().collect();
+        let values = s.random.parameters.values().collect::<Vec<&(f64, f64)>>();
+        if keys != ["Ke", "V"] {
+            println!("{}: {:?}", i, keys);
+        }
+        if values != [&(0.001, 3.0), &(25.0, 250.0)] {
+            println!("{}: {:?}", i, values);
+        }
+    }
 }
