101298a31952580f026f65a28001187821306ef5
Markus <markushh@uio.no>
2024-06-23 16:08:07 +0200

    Update styling

--- src/routines/output.rs
+++ src/routines/output.rs
@@ -7,7 +7,7 @@ use pharmsol::prelude::simulator::Equation;
 use pharmsol::prelude::simulator::Prediction;
 use settings::Settings;
 use std::fs::File;
-use tabled::{Table, Tabled};
+use tabled::{settings::Style, Table, Tabled};
 
 /// Defines the result objects from an NPAG run
 /// An [NPResult] contains the necessary information to generate predictions and summary statistics
@@ -125,51 +125,30 @@ impl NPResult {
         let aic: f64 = 2.0 * self.objf + 2.0 * self.theta.ncols() as f64;
         let bic: f64 = self.objf + self.theta.ncols() as f64 * (self.theta.nrows() as f64).ln();
 
-        tracing::info!("Model summary:");
-        tracing::info!("Final objective function value: {:.4}", self.objf);
-        tracing::info!("AIC: {:.4}", aic);
-        tracing::info!("BIC: {:.4}", bic);
-        tracing::info!("Number of support points: {}", self.theta.nrows());
+        let mut mod_table = Table::new(&[ModelSummaryRow {
+            objf: format!("{:.2}", self.objf),
+            aic: format!("{:.2}", aic),
+            bic: format!("{:.2}", bic),
+            nspp: format!("{}", self.theta.nrows()),
+        }]);
+
+        tracing::info!(
+            "{}\n{}",
+            "Model summary",
+            mod_table.with(Style::modern()).to_string()
+        );
 
         tracing::info!("Below are mean prediction error (MPE, bias) and mean absolute error (MAE, accuracy) for each output equation.");
 
-        fn create_table(
-            mean_data: &HashMap<usize, (f64, f64)>,
-            median_data: &HashMap<usize, (f64, f64)>,
-            title: &str,
-        ) -> String {
-            let mut rows = Vec::new();
-
-            for (outeq, (mpe, mae)) in mean_data {
-                rows.push(PredictionRow {
-                    outeq: *outeq,
-                    icen: "mean",
-                    mae: format!("{:.2}", mae),
-                    mpe: format!("{:.2}", mpe),
-                });
-            }
-            for (outeq, (mpe, mae)) in median_data {
-                rows.push(PredictionRow {
-                    outeq: *outeq,
-                    icen: "median",
-                    mae: format!("{:.2}", mae),
-                    mpe: format!("{:.2}", mpe),
-                });
-            }
-
-            let table = Table::new(&rows).to_string();
-            format!("{}\n{}", title, table)
-        }
-
         // Create and log the tables
-        let pop_table = create_table(
+        let pop_table = create_pred_err_table(
             &pop_mean_mpe_mae,
             &pop_median_mpe_mae,
             "Population Predictions",
         );
         tracing::info!("{}", pop_table);
 
-        let post_table = create_table(
+        let post_table = create_pred_err_table(
             &post_mean_mpe_mae,
             &post_median_mpe_mae,
             "Posterior Predictions",
@@ -709,9 +688,46 @@ fn calculate_mae(predictions: &Vec<Prediction>) -> f64 {
 }
 
 #[derive(Tabled)]
-struct PredictionRow {
+struct PredictionSummaryRow {
     outeq: usize,
     icen: &'static str,
     mae: String,
     mpe: String,
+}
+
+fn create_pred_err_table(
+    mean_data: &HashMap<usize, (f64, f64)>,
+    median_data: &HashMap<usize, (f64, f64)>,
+    title: &str,
+) -> String {
+    let mut rows = Vec::new();
+
+    for (outeq, (mpe, mae)) in mean_data {
+        rows.push(PredictionSummaryRow {
+            outeq: *outeq,
+            icen: "mean",
+            mae: format!("{:.2}", mae),
+            mpe: format!("{:.2}", mpe),
+        });
+    }
+    for (outeq, (mpe, mae)) in median_data {
+        rows.push(PredictionSummaryRow {
+            outeq: *outeq,
+            icen: "median",
+            mae: format!("{:.2}", mae),
+            mpe: format!("{:.2}", mpe),
+        });
+    }
+
+    let table = Table::new(&rows).with(Style::modern()).to_string();
+    format!("{}\n{}", title, table)
+}
+
+
+#[derive(Tabled)]
+struct ModelSummaryRow {
+    objf: String,
+    aic: String,
+    bic: String,
+    nspp: String,
 }
