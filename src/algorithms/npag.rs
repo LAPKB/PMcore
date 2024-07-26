d8c314a0432ed68c2ea32bb35eb393f5c9d7e5dc
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-07-26 23:36:55 +0100

    fix, population predictions are not generated when using SDEs

--- src/algorithms/npag.rs
+++ src/algorithms/npag.rs
@@ -270,10 +270,12 @@ impl NPAG {
 
             self.theta = self.theta.select(Axis(0), &keep);
             self.psi = self.psi.select(Axis(1), &keep);
-            self.population_predictions.subject_predictions = self
-                .population_predictions
-                .subject_predictions
-                .select(Axis(1), &keep);
+            if !self.equation.is_sde() {
+                self.population_predictions.subject_predictions = self
+                    .population_predictions
+                    .subject_predictions
+                    .select(Axis(1), &keep);
+            }
 
             //Rank-Revealing Factorization
             let (r, perm) = qr::calculate_r(&self.psi);
@@ -299,10 +301,12 @@ impl NPAG {
 
             self.theta = self.theta.select(Axis(0), &keep);
             self.psi = self.psi.select(Axis(1), &keep);
-            self.population_predictions.subject_predictions = self
-                .population_predictions
-                .subject_predictions
-                .select(Axis(1), &keep);
+            if !self.equation.is_sde() {
+                self.population_predictions.subject_predictions = self
+                    .population_predictions
+                    .subject_predictions
+                    .select(Axis(1), &keep);
+            }
 
             (self.lambda, self.objf) = match burke(&self.psi) {
                 Ok((lambda, objf)) => (lambda, objf),
