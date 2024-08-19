0139161c0a5eaa589c07ccd2c6c0e83b4b93637a
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-08-19 15:43:39 +0100

    2 comp test

--- examples/iov/main.rs
+++ examples/iov/main.rs
@@ -3,30 +3,29 @@ fn main() {
     let sde = simulator::Equation::new_sde(
         |x, p, _t, dx, _rateiv, _cov| {
             // automatically defined
-            fetch_params!(p, _ske);
-            let ke = x[2];
-            let ke0 = 0.7;
-            let ka = 0.3;
-            dx[2] = -ke + ke0;
+            fetch_params!(p, ka, ke0, _ske, kcp, kpc);
+            let ke = x[3];
+            dx[3] = -ke + ke0;
             // user defined
             dx[0] = -ka * x[0];
-            dx[1] = ka * x[0] - ke * x[1];
+            dx[1] = ka * x[0] - ke * x[1] - kcp * x[1] + kpc * x[2];
+            dx[2] = kcp * x[1] - kpc * x[2];
         },
         |p, d| {
-            fetch_params!(p, ske);
-            d[2] = ske;
+            fetch_params!(p, _ka, _ke0, ske, _kcp, _kpc);
+            d[3] = ske;
         },
         |_p| lag! {},
         |_p| fa! {},
         |p, _t, _cov, x| {
-            fetch_params!(p, _ske);
-            x[2] = 0.7
+            fetch_params!(p, _ka, ke0, _ske, _kcp, _kpc);
+            x[3] = ke0;
         },
         |x, p, _t, _cov, y| {
-            fetch_params!(p, _ske);
-            y[0] = x[1] / 50.0;
+            fetch_params!(p, _ka, _ke0, _ske, _kcp, _kpc);
+            y[0] = x[1] / 1.0;
         },
-        (3, 1),
+        (4, 1),
     );
 
     // let ode = simulator::Equation::new_ode(
@@ -47,7 +46,7 @@ fn main() {
     // );
 
     let settings = settings::read("examples/iov/config.toml".to_string()).unwrap();
-    let data = data::read_pmetrics("examples/iov/test.csv").unwrap();
+    let data = data::read_pmetrics("examples/iov/sde_data.csv").unwrap();
     let _result = fit(sde, data, settings);
     // let _result = fit(ode, data, settings);
 }
