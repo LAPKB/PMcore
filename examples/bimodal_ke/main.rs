ae49bd5bb71083413d9f7ade9925ed9d2974846f
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-09-24 13:46:01 +0100

    example net bke

--- examples/bimodal_ke/main.rs
+++ examples/bimodal_ke/main.rs
@@ -1,21 +1,21 @@
 use logger::setup_log;
 use pmcore::prelude::*;
 fn main() {
-    let eq = equation::ODE::new(
-        |x, p, _t, dx, rateiv, _cov| {
-            // fetch_cov!(cov, t, wt);
-            fetch_params!(p, ke, _v);
-            dx[0] = -ke * x[0] + rateiv[0];
-        },
-        |_p| lag! {},
-        |_p| fa! {},
-        |_p, _t, _cov, _x| {},
-        |x, p, _t, _cov, y| {
-            fetch_params!(p, _ke, v);
-            y[0] = x[0] / v;
-        },
-        (1, 1),
-    );
+    // let eq = equation::ODE::new(
+    //     |x, p, _t, dx, rateiv, _cov| {
+    //         // fetch_cov!(cov, t, wt);
+    //         fetch_params!(p, ke, _v);
+    //         dx[0] = -ke * x[0] + rateiv[0];
+    //     },
+    //     |_p| lag! {},
+    //     |_p| fa! {},
+    //     |_p, _t, _cov, _x| {},
+    //     |x, p, _t, _cov, y| {
+    //         fetch_params!(p, _ke, v);
+    //         y[0] = x[0] / v;
+    //     },
+    //     (1, 1),
+    // );
     // let eq = equation::Analytical::new(
     //     one_compartment,
     //     |_p, _t, _cov| {},
@@ -28,6 +28,11 @@ fn main() {
     //     },
     //     (1, 1),
     // );
+    let eq = equation::ODENet::new(
+        vec![dmatrix![-1.0], dmatrix![0.0]],
+        vec![OutEq::new(0, 0, Op::Div(1))],
+        (1, 1),
+    );
 
     let settings = settings::read("examples/bimodal_ke/config.toml").unwrap();
     setup_log(&settings).unwrap();
