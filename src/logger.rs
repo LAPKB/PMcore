e06133809082a14089466ed18471472dae1eb0a7
Markus <66058642+mhovd@users.noreply.github.com>
2024-10-17 14:02:20 +0200

    Log timestamp is now in elapsed time

--- src/logger.rs
+++ src/logger.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use crate::prelude::output::OutputFile;
 use crate::routines::settings::Settings;
 use anyhow::Result;
@@ -24,6 +26,10 @@ pub fn setup_log(settings: &Settings) -> Result<()> {
     let log_level = settings.log.level.as_str();
     let env_filter = EnvFilter::new(log_level);
 
+    let timestamper = CompactTimestamp {
+        start: Instant::now(),
+    };
+
     // Define a registry with that level as an environment filter
     let subscriber = Registry::default().with(env_filter);
 
@@ -34,14 +40,14 @@ pub fn setup_log(settings: &Settings) -> Result<()> {
     let file_layer = fmt::layer()
         .with_writer(outputfile.file)
         .with_ansi(false)
-        .with_timer(CompactTimestamp);
+        .with_timer(timestamper.clone());
 
     // Define layer for stdout
     let stdout_layer = fmt::layer()
         .with_writer(std::io::stdout)
         .with_ansi(true)
         .with_target(false)
-        .with_timer(CompactTimestamp);
+        .with_timer(timestamper.clone());
 
     // Combine layers with subscriber
     subscriber.with(file_layer).with(stdout_layer).init();
@@ -50,13 +56,20 @@ pub fn setup_log(settings: &Settings) -> Result<()> {
 }
 
 #[derive(Clone)]
-struct CompactTimestamp;
+struct CompactTimestamp {
+    start: Instant,
+}
 
 impl FormatTime for CompactTimestamp {
     fn format_time(
         &self,
         w: &mut tracing_subscriber::fmt::format::Writer<'_>,
     ) -> Result<(), std::fmt::Error> {
-        write!(w, "{}", chrono::Local::now().format("%H:%M:%S"))
+        let elapsed = self.start.elapsed();
+        let hours = elapsed.as_secs() / 3600;
+        let minutes = (elapsed.as_secs() % 3600) / 60;
+        let seconds = elapsed.as_secs() % 60;
+
+        write!(w, "{:02}h {:02}m {:02}s", hours, minutes, seconds)
     }
 }
