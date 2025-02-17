37b59dd478998572e791e2ece95b9dc2263af000
Julian D. Otalvaro <juliandavid347@gmail.com>
2025-02-17 18:45:56 +0000

    walter's IOV examples

---
+++ examples/w_iov/main.rs
@@ -0,0 +1,40 @@
+use pmcore::{logger::setup_log, prelude::*};
+
+fn main() {
+    let sde = equation::SDE::new(
+        |x, p, _t, dx, _rateiv, _cov| {
+            // automatically defined
+            fetch_params!(p, ke0, _ske);
+            // let ke0 = 1.2;
+            dx[1] = -x[1] + ke0;
+            let ke = x[1];
+            // user defined
+            dx[0] = -ke * x[0];
+        },
+        |p, d| {
+            fetch_params!(p, _ke0, ske);
+            d[1] = ske;
+        },
+        |_p| lag! {},
+        |_p| fa! {},
+        |p, _t, _cov, x| {
+            fetch_params!(p, ke0, _ske);
+            x[1] = ke0;
+        },
+        |x, p, _t, _cov, y| {
+            fetch_params!(p, _ke0, _ske);
+            y[0] = x[0] / 50.0;
+        },
+        (2, 1),
+        11,
+    );
+
+    let settings = settings::read("examples/w_iov/config.toml".to_string()).unwrap();
+    setup_log(&settings).unwrap();
+    let data = data::read_pmetrics("examples/w_iov/test.csv").unwrap();
+    let mut algorithm = dispatch_algorithm(settings, sde, data).unwrap();
+    algorithm.initialize().unwrap();
+    while !algorithm.next_cycle().unwrap() {}
+    let result = algorithm.into_npresult();
+    result.write_outputs().unwrap();
+}
