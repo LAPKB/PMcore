234af78388b520799d87d9fcad908f39e5339c67
Markus <markushh@uio.no>
2025-02-18 16:38:14 +0100

    Fix bimodal_ke

--- examples/bimodal_ke/main.rs
+++ examples/bimodal_ke/main.rs
@@ -6,14 +6,14 @@ fn main() -> Result<()> {
     let eq = equation::ODE::new(
         |x, p, _t, dx, rateiv, _cov| {
             // fetch_cov!(cov, t, wt);
-            fetch_params!(p, ke);
+            fetch_params!(p, ke, _v);
             dx[0] = -ke * x[0] + rateiv[0];
         },
         |_p| lag! {},
         |_p| fa! {},
         |_p, _t, _cov, _x| {},
         |x, p, _t, _cov, y| {
-            fetch_params!(p, v);
+            fetch_params!(p, _ke, v);
             y[0] = x[0] / v;
         },
         (1, 1),
--- src/algorithms/npag.rs
+++ src/algorithms/npag.rs
@@ -377,6 +377,10 @@ impl<E: Equation> NPAG<E> {
             let zero_probability_subjects: Vec<&String> =
                 indices.iter().map(|&i| subject[i].id()).collect();
 
+            dbg!(&self.theta);
+
+            dbg!(&psi);
+
             return Err(anyhow::anyhow!(
                 "The probability of one or more subjects, given the model, is zero. The following subjects have zero probability: {:?}", zero_probability_subjects
             ));
