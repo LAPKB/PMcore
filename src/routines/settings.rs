a7a761b49d6644323702abaa7b82adcfcc6469c1
Markus <markushh@uio.no>
2025-03-04 15:30:44 +0100

    Revert "Simplify the Parameters structure"
    This reverts commit 0bc130cd8e1efcad5ee585fe05b292aa35ca3a36.

--- src/routines/settings.rs
+++ src/routines/settings.rs
@@ -271,7 +271,7 @@ impl Parameter {
 /// This structure contains information on all [Parameter]s to be estimated
 #[derive(Debug, Clone, Deserialize, Serialize, Default)]
 pub struct Parameters {
-    parameters: Vec<Parameter>,
+    parameters: Vec<(String, Parameter)>,
 }
 
 impl Parameters {
@@ -290,19 +290,30 @@ impl Parameters {
     // Get a parameter by name
     pub fn get(&self, name: impl Into<String>) -> Option<&Parameter> {
         let name = name.into();
-        self.parameters.iter().find(|p| p.name == name)
+        self.parameters
+            .iter()
+            .find(|(n, _)| n == &name)
+            .map(|(_, p)| p)
     }
 
     /// Get the names of the parameters
     pub fn names(&self) -> Vec<String> {
-        self.parameters.iter().map(|p| p.name.clone()).collect()
+        self.parameters
+            .iter()
+            .map(|(name, _)| name.clone())
+            .collect()
     }
+
     /// Get the ranges of the parameters
     ///
     /// Returns a vector of tuples, where each tuple contains the lower and upper bounds of the parameter
     pub fn ranges(&self) -> Vec<(f64, f64)> {
-        self.parameters.iter().map(|p| (p.lower, p.upper)).collect()
+        self.parameters
+            .iter()
+            .map(|(_, p)| (p.lower, p.upper))
+            .collect()
     }
+
     pub fn len(&self) -> usize {
         self.parameters.len()
     }
@@ -311,23 +322,23 @@ impl Parameters {
         self.parameters.is_empty()
     }
 
-    pub fn iter(&self) -> std::slice::Iter<'_, Parameter> {
+    pub fn iter(&self) -> std::slice::Iter<'_, (String, Parameter)> {
         self.parameters.iter()
     }
 }
 
 impl IntoIterator for Parameters {
-    type Item = Parameter;
-    type IntoIter = std::vec::IntoIter<Parameter>;
+    type Item = (String, Parameter);
+    type IntoIter = std::vec::IntoIter<(String, Parameter)>;
 
     fn into_iter(self) -> Self::IntoIter {
         self.parameters.into_iter()
     }
 }
-/// Builder for creating a set of parameters
+
 /// Builder for creating a set of parameters
 pub struct ParametersBuilder {
-    parameters: Vec<Parameter>,
+    parameters: Vec<(String, Parameter)>,
 }
 
 impl ParametersBuilder {
@@ -339,8 +350,8 @@ impl ParametersBuilder {
 
     pub fn add(mut self, name: impl Into<String>, lower: f64, upper: f64, fixed: bool) -> Self {
         let name_string = name.into();
-        if let Ok(parameter) = Parameter::new(name_string, lower, upper, fixed) {
-            self.parameters.push(parameter);
+        if let Ok(parameter) = Parameter::new(&name_string, lower, upper, fixed) {
+            self.parameters.push((name_string, parameter));
         }
         self
     }
@@ -351,6 +362,7 @@ impl ParametersBuilder {
         })
     }
 }
+
 /// Defines the error model and polynomial to be used
 #[derive(Debug, Deserialize, Clone, Serialize)]
 #[serde(deny_unknown_fields, default)]
