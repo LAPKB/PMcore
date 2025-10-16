f1872b793daf9dd6d22cd7d0d275f8eeb4527f31
Walter Mas Yamada <walter.mas.yamada@gmail.com>
2025-10-16 10:05:10 -0700

    2025 Oct 16 preparation to work with Julian

--- examples/sde_paper/main.rs
+++ examples/sde_paper/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+// use std::intrinsics;
 use std::path::Path;
 
 use pharmsol::equation;
@@ -162,6 +163,8 @@ fn model_ke_v_s1_s2() -> equation::SDE {
             // let v = x[2];
             // user defined
             dx[0] = -ke * x[0];
+            // ODE
+            dx[3] = -ke * x[3];
         },
         |p, d| {
             fetch_params!(p, _ke0, _v0, s1, s2);
@@ -175,22 +178,24 @@ fn model_ke_v_s1_s2() -> equation::SDE {
             x[0] = 20.0;
             x[1] = k0;
             x[2] = v0;
+            x[3] = 20.0; // ODE -- Like = ODE + SDE
         },
         |x, p, _t, _cov, y| {
-            fetch_params!(p, _k0, _v0, _s1, _s2);
+            fetch_params!(p, _k0, v0, _s1, _s2);
             let v = x[2];
             y[0] = x[0] / v;
+            y[1] = x[3] / v0;
         },
-        (3, 1),
-        1,
+        (4, 2),
+        47,
     )
 }
 
 fn settings_exp1() -> Settings {
     let mut settings = Settings::new();
     let params = Parameters::builder()
-        .add("ke0", 0.1, 3.0, false) // Range covers both normal distributions (0.5±0.05 and 1.5±0.15)
-        .add("v0", 0.5, 2.0, false) 
+        .add("ke0", 2.5e-2, 1.5 + 5.0 * (1.5 - (0.5 + 3.0 * 0.05))/3.0, false) // Range covers both normal distributions (0.5±0.05 and 1.5±0.15)
+        .add("v0", 0.0001, 2.0, false) 
         .build()
         .unwrap();
 
@@ -201,9 +206,9 @@ fn settings_exp1() -> Settings {
 fn settings_exp2() -> Settings {
     let mut settings = Settings::new();
     let params = Parameters::builder()
-        .add("ke0", 0.1, 3.0, false) // Base ke range
-        .add("v0", 0.8, 1.2, false)
-         .add("s1", 0.0001, 0.075, false)        // Diffusion parameter for ke
+        .add("ke0", 1.0e-7, 3.5, false) // Base ke range
+        .add("v0", 0.2, 1.8, false)
+         .add("s1", 0.01, 2.0, false)        // Diffusion parameter for ke
         .build()
         .unwrap();
 
@@ -239,10 +244,10 @@ fn settings_exp4() -> Settings {
 fn settings_exp5() -> Settings {
     let mut settings = Settings::new();
     let params = Parameters::builder()
-        .add("ke0", 0.1, 3.0, false)
-        .add("v0", 0.5, 2.0, false) // Base volume range
-        .add("s1", 2.5e-2, 0.3, false) // Diffusion parameter for ke
-        .add("s2", 0.1, 0.4, false) // Diffusion parameter for volume
+        .add("ke0", 2.5e-2, 1.5 + 5.0 * (1.5 - (0.5 + 3.0 * 0.05))/3.0, false)
+        .add("v0", 0.0001, 2.0, false) // Base volume range
+        .add("s1", 0.0025, 5.4, false) // Diffusion parameter for ke
+        .add("s2", 0.01, 0.4, false) // Diffusion parameter for volume
         .build()
         .unwrap();
 
@@ -257,7 +262,7 @@ fn setup_common_settings(settings: &mut Settings) -> Settings {
     settings.set_error_type(ErrorType::Add);
     settings.set_prior(Prior {
         sampler: "sobol".to_string(),
-        points: 2449,
+        points: 4851,
         seed: 347,
         file: None,
     });
@@ -287,7 +292,9 @@ fn sample_k0(rng: &mut StdRng, n1: Normal<f64>, n2: Normal<f64>) -> f64 {
 }
 
 fn sample_v(rng: &mut StdRng, n3: Normal<f64>) -> f64 {
-    1.0 + 0.2 * n3.sample(rng) // n3 ~ N(0,1)
+    {
+        (1.0 + 0.2 * n3.sample(rng)).abs()
+    } // n3 ~ N(0,1)
 }
 
 fn write_samples_to_file(
@@ -324,8 +331,8 @@ fn generate_data() {
     //k0 dist according to the paper F(K0) = 0.5*N(m_1,(s_1)^2) + 0.5*N(m_2,(s_2)^2)
     let m1 = 0.5;
     let s1 = 0.05;
-    let m2 = 1.5;
-    let s2 = 0.15;
+    let m2 = 1.50;
+    let s2 = (m2 - (m1 + 3.0 * s1)) / 3.0; // let s2 = 0.15;
     let n1 = Normal::new(m1, s1).unwrap();
     let n2 = Normal::new(m2, s2).unwrap();
     let mut rng = rand::rngs::StdRng::seed_from_u64(347);
@@ -379,7 +386,7 @@ fn generate_data() {
         "examples/sde_paper/data/experiment2.csv",
         k0_pop.clone(),
         None,
-        0.05,
+        1.0e-3,
         0.0,
         model_ke_s1(),
     );
@@ -409,8 +416,8 @@ fn generate_data() {
         "examples/sde_paper/data/experiment5.csv",
         k0_pop.clone(),
         Some(v_pop.clone()),
-        0.05, // mu <- c(0.5 +/- 0.05) or mu <- c(1.5 +/- 0.15)
-        0.20, // sigma_vol is hard coded to 0.2 => 20%CV, vol = 1+/-s
+        0.5346338, // (ke_s1^2 + ke_s2^2)^0.5 = 0.5346338
+        0.2, // sigma_vol is hard coded to 0.2 => 20%CV
         model_ke_v_s1_s2(),
     );
 }
@@ -419,11 +426,12 @@ fn fit_experiment(experiment: usize) {
     // Experiment 0 is the ODE model // note: optimize on exp#5, which has stochastic r.v.s
 
     if experiment == 0 {
-        let data = data::read_pmetrics("examples/sde_paper/data/experiment2.csv").unwrap();
+        let data = data::read_pmetrics("examples/sde_paper/data/experiment5.csv").unwrap();
         let eqn = model_ke_ode();
         let mut settings = settings_exp1();
         settings.set_output_path("examples/sde_paper/output/experiment0");
-        settings.set_error_poly((4.0, 0.32, 0.0, 0.0));
+        // settings.set_error_poly((4.0, 0.32, 0.0, 0.0)); // => shrinkage, a lot!
+        settings.set_error_poly((0.0, 0.01, 0.0, 0.0));
         let mut problem = dispatch_algorithm(settings, eqn, data).unwrap();
         let result = problem.fit().unwrap();
         result.write_outputs().unwrap();
@@ -451,8 +459,8 @@ fn fit_experiment(experiment: usize) {
     ));
 
     settings.set_error_poly(match experiment {
-        1..=4 => (0.05, 0.1, 0.0, 0.0),
-        5 => (0.0, 0.1, 0.0, 0.0),
+        1..=4 => (0.0, 0.1, 0.0, 0.0),
+        5 => (0.0, 0.01, 0.0, 0.0),
         _ => panic!("Invalid experiment"),
     });
 
@@ -464,7 +472,7 @@ fn fit_experiment(experiment: usize) {
 fn main() {
     generate_data();
     fit_experiment(0);
-    fit_experiment(2);
+    fit_experiment(5);
     /*
     for i in 0..=2 { // 0..=5 runs all of them
         println!("Start experiment {}",i);
