d2cbab2b9d8a0ff03b086455fc8f6b2edf5dd295
Markus <markushh@uio.no>
2025-11-12 19:35:39 +0100

    chore: Update field name in NPPredictionrow to cens
    Instead of censoring, more in line with NONMEM etc.

--- src/routines/output/predictions.rs
+++ src/routines/output/predictions.rs
@@ -21,7 +21,7 @@ pub struct NPPredictionRow {
     /// The observed value, if any
     obs: Option<f64>,
     /// Censored observation flag
-    censoring: Censor,
+    cens: Censor,
     /// The population mean prediction
     pop_mean: f64,
     /// The population median prediction
@@ -216,7 +216,7 @@ impl NPPredictions {
                         outeq: p.outeq(),
                         block: p.occasion(),
                         obs: p.observation(),
-                        censoring: p.censoring(),
+                        cens: p.censoring(),
                         pop_mean: pop_mean[j],
                         pop_median: pop_median[j],
                         post_mean: posterior_mean[j],
